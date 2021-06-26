399dd142d8f4d7d2be35187e4e98aeed5a66cbdd
João Pedro <jpedro1580@gmail.com>
2021-06-26 08:41:07 -0300

    Load plugins and colorscheme on startup

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
-require('sets')
 vim.cmd'source ~/.config/nvim/myplugins.vim'
+vim.cmd'colorscheme dracula'
+require('sets')
+require('bindings')
+require('nvim-autopairs').setup()
+require('nvim-treesitter').setup()
+require('nvim_comment').setup()
+require('nv-compe')
+
+require('lsp')
+require('lsp.tsserver')
