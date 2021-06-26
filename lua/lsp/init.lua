ef09b1c6e0482346c1157c460884471a539a8ff0
João Pedro <jpedro1580@gmail.com>
2021-06-26 10:37:12 -0300

    New Keymappings for lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -16,10 +16,25 @@ vim.fn.sign_define(
     {texthl = "LspDiagnosticsDefaultInformation", text = "", numhl = "LspDiagnosticsDefaultInformation"}
 )
 
-vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
-vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
-vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
-vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
+local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+local opts = {noremap=true,silent=true} 
+buf_set_keymap('n','gd',' <cmd>lua vim.lsp.buf.definition()<CR>',opts)
+buf_set_keymap('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>',opts)
+buf_set_keymap('n','gr','<cmd>lua vim.lsp.buf.references()<CR>',opts)
+buf_set_keymap('n','gr','<cmd>lua vim.lsp.buf.references()<CR>', opts)
+buf_set_keymap('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>',opts)
+buf_set_keymap('n','<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+buf_set_keymap('n','<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+buf_set_keymap('n','<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+buf_set_keymap('n','<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+buf_set_keymap('n','<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+buf_set_keymap('n','<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+buf_set_keymap('n','<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+buf_set_keymap('n','[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+buf_set_keymap('n',']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+buf_set_keymap('n','<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+buf_set_keymap("n","<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 -- Set Default Prefix.
 -- Note: You can set a prefix per lsp server in the lv-globals.lua file
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
