f45fdfe5a6ca4dd20b2bd8c1b38cca726d3156ab
João Pedro <jpedro1580@gmail.com>
2021-07-02 04:26:34 -0300

    You're writing lua, remeber that
    Also. Remove list of mappings because it shouldn't be there

--- lua/nv-tree.lua
+++ lua/nv-tree.lua
@@ -1,16 +1,16 @@
 vim.g.nvim_tree_width = 30
-vim.g.nvim_tree_gitignore = 1 
-vim.g.nvim_tree_follow = 1 
-vim.g.nvim_tree_hide_dotfiles = 1 
-vim.g.nvim_tree_git_hl = 1 
-vim.g.nvim_tree_highlight_opened_files = 1 
-vim.g.nvim_tree_tab_open = 1 
-vim.g.nvim_tree_width_allow_resize  = 1 
-vim.g.nvim_tree_disable_netrw = 1 
-vim.g.nvim_tree_hijack_netrw = 1 
-vim.g.nvim_tree_add_trailing = 1 
-vim.g.nvim_tree_group_empty = 1 
-vim.g.nvim_tree_lsp_diagnostics = 1 
+vim.g.nvim_tree_gitignore = true 
+vim.g.nvim_tree_follow = true 
+vim.g.nvim_tree_hide_dotfiles = true 
+vim.g.nvim_tree_git_hl = true 
+vim.g.nvim_tree_highlight_opened_files = true 
+vim.g.nvim_tree_tab_open = true 
+vim.g.nvim_tree_width_allow_resize  = true 
+vim.g.nvim_tree_disable_netrw = true 
+vim.g.nvim_tree_hijack_netrw = true 
+vim.g.nvim_tree_add_trailing = true 
+vim.g.nvim_tree_group_empty = true 
+vim.g.nvim_tree_lsp_diagnostics = true 
 vim.g.nvim_tree_auto_open = true
 vim.g.nvim_tree_window_picker_exclude = {
         filetype = {
@@ -23,39 +23,4 @@ vim.g.nvim_tree_window_picker_exclude = {
      }
 
 vim.g.nvim_tree_special_files = { 'README.md', 'Makefile', 'MAKEFILE' } 
-vim.g.nvim_tree_show_icons = { git= 1, folders= 1, files= 1, folder_arrows= 1 }
-
-local tree_cb = require('nvim-tree.config').nvim_tree_callback
- vim.g.nvim_tree_bindings = {
-      { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-      { key = {"<2-RightMouse>", "<C-}>"},    cb = tree_cb("cd") },
-      { key = "v",                        cb = tree_cb("vsplit") },
-      { key = "x",                        cb = tree_cb("split") },
-      { key = "t",                        cb = tree_cb("tabnew") },
-      { key = "<",                            cb = tree_cb("prev_sibling") },
-      { key = ">",                            cb = tree_cb("next_sibling") },
-      { key = "P",                            cb = tree_cb("parent_node") },
-      { key = "<BS>",                         cb = tree_cb("close_node") },
-      { key = "<S-CR>",                       cb = tree_cb("close_node") },
-      { key = "<Tab>",                        cb = tree_cb("preview") },
-      { key = "K",                            cb = tree_cb("first_sibling") },
-      { key = "J",                            cb = tree_cb("last_sibling") },
-      { key = "I",                            cb = tree_cb("toggle_ignored") },
-      { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-      { key = "R",                            cb = tree_cb("refresh") },
-      { key = "a",                            cb = tree_cb("create") },
-      { key = "d",                            cb = tree_cb("remove") },
-      { key = "r",                            cb = tree_cb("rename") },
-      { key = "<C->",                         cb = tree_cb("full_rename") },
-      { key = "x",                            cb = tree_cb("cut") },
-      { key = "c",                            cb = tree_cb("copy") },
-      { key = "p",                            cb = tree_cb("paste") },
-      { key = "y",                            cb = tree_cb("copy_name") },
-      { key = "Y",                            cb = tree_cb("copy_path") },
-      { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-      { key = "[c",                           cb = tree_cb("prev_git_item") },
-      { key = "}c",                           cb = tree_cb("next_git_item") },
-      { key = "-",                            cb = tree_cb("dir_up") },
-      { key = "q",                            cb = tree_cb("close") },
-      { key = "g?",                           cb = tree_cb("toggle_help") },
-    }
+vim.g.nvim_tree_show_icons = { git= true, folders= true, files= true, folder_arrows= true }
