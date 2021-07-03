52a7e999057b611936fca6f09259bec1fea3f5b1
João Pedro <jpedro1580@gmail.com>
2021-07-03 01:23:37 -0300

    I've read a out of date documentation. Had to rewrite all of the options to what it was before

--- lua/sets.lua
+++ lua/sets.lua
@@ -1,16 +1,16 @@
 -- Helpers
-local o = vim.opt
---vim.cmd('filetype on')
---vim.cmd('syntax enable')
+local o = vim.o
+local bo = vim.bo
+local wo = vim.wo
+vim.cmd('filetype on')
 
 o.tgc = true
 o.enc = "utf-8"
 o.splitbelow = true
 o.clipboard = "unnamedplus"
-o.path:append("**")
+o.path=".,/usr/include,,**"
 o.incsearch = true
 o.hlsearch = false
-o.signcolumn='yes'
 o.mouse = "a"
 o.pumheight = 12
 o.pumwidth = 20
@@ -18,14 +18,15 @@ o.scrolloff = 10
 o.cmdheight = 2
 o.hidden = true
 o.inccommand = "split"
-o.ts = 4
-o.expandtab=true
-o.swapfile = false
-o.spelllang = "pt"
-o.rnu = true
 o.cursorline = true
 o.completeopt = "menuone,noselect"
-o.formatoptions:remove({'c','r','o'}) 
+bo.ts = 4
+bo.swapfile = false
+bo.spelllang = "pt"
+vim.cmd('set formatoptions -=cro') 
+wo.signcolumn='yes'
+wo.rnu = true
+wo.number = true
 
 -- Plugin specific opttions
 vim.g.simple_latex_viewer = 'okular'
