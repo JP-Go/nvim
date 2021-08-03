2414ea513565eb06805ddd3de4db3baf387c3d43
João Pedro <jpedro@gmail.com>
2021-08-03 13:32:11 -0300

    Configure rust lsp

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -6,5 +6,4 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 require('lspconfig').rust_analyzer.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = { 'rust' },
 })
