41a56311a44b1fc1987ab65766a86c48df8590fb
João Pedro <jpedro@gmail.com>
2021-09-10 15:41:52 -0300

    Set autopairs to not complete functions in latex

--- lua/nv-autopairs.lua
+++ lua/nv-autopairs.lua
@@ -1,10 +1,15 @@
 local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 local cond = require('nvim-autopairs.conds')
+local complete_function = function()
+    local ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    return ft ~= 'tex'
+end
 
 require('nvim-autopairs.completion.compe').setup({
     map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` after select function or method item
+    map_complete = complete_function(), -- it will auto insert `(` after select function or method item
+    enable_check_bracket_line = false,
 })
 
 npairs.setup()
@@ -15,5 +20,11 @@ npairs.add_rules({
         :with_move(cond.none()) -- don't delete if the next character is xx
         :with_del(cond.not_after_regex_check('xx'))
         :with_cr(cond.none()),
-    Rule('\\[', '\\]', { 'tex', 'latex' }):with_pair(cond.not_after_regex_check('%%')),
+    Rule('\\[', '\\]', 'tex'):with_pair(function(opts)
+        print(vim.inspect(opts))
+        if opts.line == 'aa \\[' then
+            -- don't add pair on that line
+            return false
+        end
+    end),
 })
