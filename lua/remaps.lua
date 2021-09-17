aa16ea593e35ca80f205f793d1ba47aad446a72b
João Pedro <jpedro@gmail.com>
2021-09-17 11:44:53 -0300

    Refactor of functions name

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,70 +1,70 @@
-local bindkeys = vim.api.nvim_set_keymap
+local remap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 -- Remappign space as leader key
 vim.g.mapleader = ' '
 
 -- Source files
-bindkeys('n', '<leader>x', ':lua _G.execute_line()<CR>', { noremap = true })
-bindkeys('n', '<leader><leader>x', ':lua _G.source_file()<CR>', { noremap = true })
+remap('n', '<leader>x', ':lua _G.execute_line()<CR>', { noremap = true })
+remap('n', '<leader><leader>x', ':lua _G.source_file()<CR>', { noremap = true })
 
 -- Shift lines in visual mode
-bindkeys('v', '>', '>gv', opts)
-bindkeys('v', '<', '<gv', opts)
+remap('v', '>', '>gv', opts)
+remap('v', '<', '<gv', opts)
 
--- Open terminal
-bindkeys('n', '<C-t>', ':vsplit | term<CR>', opts)
+-- Open terminal window
+remap('n', '<C-t>', ':vsplit | term<CR>', opts)
 
 -- hlsearch toggle
-bindkeys('n', '<leader>h', ':set hlsearch!<CR>', opts)
+remap('n', '<leader>h', ':set hlsearch!<CR>', opts)
 
 -- Window Navigation normal mode
-bindkeys('n', '<C-h>', '<C-w>h<CR>', opts)
-bindkeys('n', '<C-j>', '<C-w>j<CR>', opts)
-bindkeys('n', '<C-k>', '<C-w>k<CR>', opts)
-bindkeys('n', '<C-l>', '<C-w>l<CR>', opts)
+remap('n', '<C-h>', '<C-w>h<CR>', opts)
+remap('n', '<C-j>', '<C-w>j<CR>', opts)
+remap('n', '<C-k>', '<C-w>k<CR>', opts)
+remap('n', '<C-l>', '<C-w>l<CR>', opts)
 
 -- Move lines with alt+jk
-bindkeys('n', '<A-j>', ':m .+1<CR>==', opts)
-bindkeys('n', '<A-k>', ':m .-2<CR>==', opts)
-bindkeys('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
-bindkeys('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
-bindkeys('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
-bindkeys('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
+remap('n', '<A-j>', ':m .+1<CR>==', opts)
+remap('n', '<A-k>', ':m .-2<CR>==', opts)
+remap('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
+remap('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
+remap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', opts)
+remap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', opts)
 
 -- Alternative to exit insert mode
-bindkeys('i', 'jj', '<Escape>', opts)
+remap('i', 'jj', '<Escape>', opts)
 
 -- Control + s to save
-bindkeys('n', '<C-s>', ':w<CR>', { noremap = true })
+remap('n', '<C-s>', ':w<CR>', { noremap = true })
 
 -- Exit terminal mode
-bindkeys('t', '<Esc>', '<C-\\><C-N>', opts)
+remap('t', '<Esc>', '<C-\\><C-N>', opts)
 
 -- Cycle trough buffers
-bindkeys('n', '<tab>', ':bNext<CR>', opts)
-bindkeys('n', '<s-tab>', ':bprevious<CR>', opts)
-bindkeys('n', '<C-q>', ':bdelete<CR>', opts)
+remap('n', '<tab>', ':bNext<CR>', opts)
+remap('n', '<s-tab>', ':bprevious<CR>', opts)
+remap('n', '<C-q>', ':bdelete<CR>', opts)
 
 -- Telescope
-bindkeys(
+remap(
     'n',
     '<leader>ff',
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_ivy())<CR>",
     { noremap = true }
 )
-bindkeys(
+remap(
     'n',
     '<leader>fg',
     "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_ivy())<CR>",
     { noremap = true }
 )
-bindkeys(
+remap(
     'n',
     '<leader>fb',
     "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_ivy())<CR>",
     { noremap = true }
 )
-bindkeys(
+remap(
     'n',
     '<leader>fh',
     "<cmd>lua require('telescope.builtin').help_tags(require('telescope.themes').get_ivy())<CR>",
@@ -72,6 +72,6 @@ bindkeys(
 )
 
 -- Nvim tree
-bindkeys('n', '<leader>e', '<cmd> NvimTreeToggle<CR>', opts)
-bindkeys('n', '<leader>r', '<cmd> NvimTreeRefresh<CR>', opts)
-bindkeys('n', '<leader>n', '<cmd> NvimTreeFindFile<CR>', opts)
+remap('n', '<leader>e', '<cmd> NvimTreeToggle<CR>', opts)
+remap('n', '<leader>r', '<cmd> NvimTreeRefresh<CR>', opts)
+remap('n', '<leader>n', '<cmd> NvimTreeFindFile<CR>', opts)
