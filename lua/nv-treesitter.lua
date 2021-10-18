b58017666c6c81e609f0e008d456dbd8bdf1ee7f
João Pedro <jpedro@gmail.com>
2021-10-18 01:14:34 -0300

    Eyecandy: raindow delimiters

--- lua/nv-treesitter.lua
+++ lua/nv-treesitter.lua
@@ -1,12 +1,14 @@
 require('nvim-treesitter.configs').setup({
     highlight = {
         enable = true,
-        disable = { 'tex', 'latex' },
     },
     indent = {
         enable = true,
     },
     autotag = {
+        enable = true,
+    },
+    rainbow = {
         enable = true,
     },
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,6 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
-
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
 if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
     execute('packadd packer.nvim')
@@ -83,4 +81,5 @@ return require('packer').startup(function()
         end,
     })
     use({ 'ThePrimeagen/vim-be-good' })
+    use({ 'p00f/nvim-ts-rainbow', after = 'nvim-treesitter' })
 end)
