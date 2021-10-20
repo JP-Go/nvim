7f0c6cbbcc3c2e8738145f132c48db5b3547b1a2
João Pedro <jpedro@gmail.com>
2021-10-20 01:32:19 -0300

    Disable treesitter for latex

--- lua/nv-treesitter.lua
+++ lua/nv-treesitter.lua
@@ -1,9 +1,11 @@
 require('nvim-treesitter.configs').setup({
     highlight = {
         enable = true,
+        disable = { 'latex', 'tex' },
     },
     indent = {
         enable = true,
+        disable = { 'latex', 'tex' },
     },
     autotag = {
         enable = true,
