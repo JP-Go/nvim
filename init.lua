9f25fa0e5acb37735879e9f3b121cb0714975cda
João Pedro <jpedro@gmail.com>
2021-10-29 16:28:57 -0300

    new colorscheme: tomorrow-night

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require('remaps')
 require('autocmds')
 require('lsps')
 require('lualine').setup({
-    options = { theme = 'onedark' },
+    options = { theme = 'codedark' },
 })
-vim.cmd([[colorscheme onedark]])
+vim.cmd([[colorscheme tomorrow-night-bright]])
