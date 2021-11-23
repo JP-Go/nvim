2dee286be15ec7674bdf0a90af434243ef96dd5b
João Pedro <jpedro@gmail.com>
2021-11-23 10:57:15 -0300

    Add lsp for the R programming language

--- lua/lsps.lua
+++ lua/lsps.lua
@@ -48,6 +48,7 @@ local servers = {
     'pyright',
     'rust_analyzer',
     'solargraph',
+    'r_language_server',
 }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
