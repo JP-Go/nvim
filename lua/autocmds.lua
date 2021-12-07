fa4afe7e4fe8a49864a4c977fac13c87a7136b7a
João Pedro <jpedro@gmail.com>
2021-12-07 20:53:54 -0300

    Add formatting to mjs files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,7 +2,7 @@ vim.api.nvim_exec(
     [[
         augroup formater
         au!
-        au BufWritePost *.js,*.ts,*.jsx,*.tsx,*.py,*.lua,*.rs,*.R FormatWrite
+        au BufWritePost *.mjs,*.js,*.ts,*.jsx,*.tsx,*.py,*.lua,*.rs,*.R FormatWrite
         augroup END
 ]],
     false
