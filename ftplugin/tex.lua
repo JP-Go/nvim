1ca1d2430a7747d3095de31aaeb025db6a1c2ebc
João Pedro <jpedro@gmail.com>
2021-12-13 23:51:24 -0300

    Quick highlight and emphasize in latex files

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,3 +1,6 @@
 vim.bo.textwidth = 80
 vim.opt.spell = true
 vim.opt.spelllang = vim.opt.spelllang + 'en'
+
+vim.api.nvim_buf_set_keymap('v', '<leader>i', 'c\textit{<ESC>pa}<ESC>', { noremap = true })
+vim.api.nvim_buf_set_keymap('v', '<leader>b', 'c\textbf{<ESC>pa}<ESC>', { noremap = true })
