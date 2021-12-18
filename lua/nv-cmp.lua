41e44a0b14cc2cbdcc5c2ceb1fe064a461228e4b
João Pedro <jpedro@gmail.com>
2021-12-18 15:03:57 -0300

    Configure Enter key to do not expand completion options

--- lua/nv-cmp.lua
+++ lua/nv-cmp.lua
@@ -9,7 +9,7 @@ cmp.setup({
     },
     mapping = {
         ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
