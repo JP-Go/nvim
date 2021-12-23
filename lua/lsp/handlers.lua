baa598be3e7d82f3836db17b3dbdf7343f314a81
João Pedro <jpedro@gmail.com>
2021-12-23 14:38:58 -0300

    feat(lsp): create setup function for prepare lsp
    Create a setup function that prepares neovim for lsp interactions
    such as document highlighting and diagnostics hinting.
    
    (Shamelessly stolen from LunarVim/Neovim-from-scratch repo, 06-LSP
    branch)

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -1,5 +1,63 @@
 local M = {}
 
+M.setup = function()
+    local signs = {
+        { name = 'DiagnosticSignError', text = '' },
+        { name = 'DiagnosticSignWarn', text = '' },
+        { name = 'DiagnosticSignHint', text = '' },
+        { name = 'DiagnosticSignInfo', text = '' },
+    }
+
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+    end
+
+    local config = {
+        -- disable virtual text
+        virtual_text = false,
+        -- show signs
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = 'minimal',
+            border = 'rounded',
+            source = 'always',
+            header = '',
+            prefix = '',
+        },
+    }
+
+    vim.diagnostic.config(config)
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = 'rounded',
+    })
+
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = 'rounded',
+    })
+end
+
+local highlight_document = function(client)
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec(
+            [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+            false
+        )
+    end
+end
+
 local apply_lsp_mappings = function(bufnr)
     local opts = { noremap = true, silent = true }
     local function buf_set_keymap(...)
@@ -8,27 +66,28 @@ local apply_lsp_mappings = function(bufnr)
     vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
 
     buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('v', '<leader>ca', ':<C-U>lua vim.lsp.buf.range_code_action()<CR>', opts)
+    buf_set_keymap('v', '<leader>ca', '<cmd><C-U>lua vim.lsp.buf.range_code_action()<CR>', opts)
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev({border = "rounded"})<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next({border = "rounded"})<CR>', opts)
+    buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help({border="rounded"})<CR>', opts)
     buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<leader>wa', ':lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wr', ':lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wl', ':lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<leader>D', ':lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<leader>sd', ':lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '<leader>ql', ':lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<leader>cf', ':Format<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>sd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border="rounded"})<CR>', opts)
+    buf_set_keymap('n', '<leader>ql', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<leader>cf', '<cmd>Format<CR>', opts)
 end
 
 M.common_on_attach = function(client, bufnr)
     apply_lsp_mappings(bufnr)
+    highlight_document(client)
 end
 
 M.servers = {
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,5 @@
 local handlers = require('lsp.handlers')
-local settings = require('lsp.settings')
-
 local servers = handlers.servers
-local common_on_attach = handlers.common_on_attach
-local common_capabilities = handlers.common_capabilities
 
 local lspconfig_status_ok, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_status_ok then
@@ -11,17 +7,19 @@ if not lspconfig_status_ok then
     return
 end
 
+handlers.setup()
+
 for _, server in ipairs(servers) do
     local options = {
-        on_attach = common_on_attach,
-        capabilities = common_capabilities,
+        on_attach = handlers.common_on_attach,
+        capabilities = handlers.common_capabilities,
     }
     if server == 'jdtls' then
-        local jdtls_configs = settings.jdtls
+        local jdtls_configs = require('lsp.settings.jdtls')
         options = vim.tbl_deep_extend('force', options, jdtls_configs)
     end
     if server == 'sumneko_lua' then
-        local sumneko_configs = settings.sumneko
+        local sumneko_configs = require('lsp.settings.sumneko')
         options = vim.tbl_deep_extend('force', options, sumneko_configs)
     end
 
