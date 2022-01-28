4e6c133533ced81c5a4b36e9f909a2183d6353d0
João Pedro <jpedro@gmail.com>
2022-01-27 22:39:32 -0300

    fix(formatter): Fix error with prettier config

--- lua/formatter-config/javascript.lua
+++ lua/formatter-config/javascript.lua
@@ -2,7 +2,13 @@ return {
   function()
     return {
       exe = "prettier",
-      args = { "--print-width=90", "--tab-width=2", "--trailing-comma=all" },
+      args = {
+        "--stdin-filepath",
+        vim.fn.fnamescape(vim.api.nvim_buf_get_name(0)),
+        "--print-width=90",
+        "--tab-width=2",
+        "--trailing-comma=all",
+      },
       stdin = true,
     }
   end,
