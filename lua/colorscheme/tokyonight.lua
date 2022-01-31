fcf0b776db7980026d76a54200ed349bccc0d0e8
João Pedro <jpedro@gmail.com>
2022-01-31 16:32:13 -0300

    noop

--- lua/colorscheme/tokyonight.lua
+++ lua/colorscheme/tokyonight.lua
@@ -1,10 +1,10 @@
 return {
-	predicate = "tokyonight",
-	separator = "_",
-	vars = {
-		style = "night",
-		sidebars = { "qf", "terminal", "packer", "NvimTree" },
-		lualine_bold = true,
-	},
-	theme = "tokyonight",
+  predicate = "tokyonight",
+  separator = "_",
+  vars = {
+    style = "night",
+    sidebars = { "qf", "terminal", "packer", "NvimTree" },
+    lualine_bold = true,
+  },
+  theme = "tokyonight",
 }
