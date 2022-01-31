2d102fafdde64969e980808295cdfb71254cd21f
João Pedro <jpedro@gmail.com>
2022-01-31 13:49:09 -0300

    fix(formatter): Fix typo in config

--- lua/formatter-config/javascript.lua
+++ lua/formatter-config/javascript.lua
@@ -4,7 +4,7 @@ return {
       exe = "prettier",
       args = {
         "--stdin-filepath",
-        vim.fn.fnamescape(vim.api.nvim_buf_get_name(0)),
+        vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
         "--print-width=90",
         "--tab-width=2",
         "--trailing-comma=all",
