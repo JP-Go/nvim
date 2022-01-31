8e565e8b647d34035d18eb8e2d0a31826bfc1e2b
João Pedro <jpedro@gmail.com>
2022-01-31 15:53:05 -0300

    refactor(lsp.handlers): Remove deprecated functions in keymappings

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -1,108 +1,108 @@
 local M = {}
 
 M.setup = function()
-	local signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
 
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+  end
 
-	local config = {
-		-- disable virtual text
-		virtual_text = false,
-		-- show signs
-		signs = {
-			active = signs,
-		},
-		update_in_insert = true,
-		underline = true,
-		severity_sort = true,
-		float = {
-			focusable = false,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	}
+  local diagnosics_win_config = {
+    -- disable virtual text
+    virtual_text = false,
+    -- show signs
+    signs = {
+      active = signs,
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
 
-	vim.diagnostic.config(config)
+  vim.diagnostic.config(diagnosics_win_config)
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "rounded",
+  })
 
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
+  })
 end
 
 local highlight_document = function(client)
-	if client.resolved_capabilities.document_highlight then
-		vim.api.nvim_exec(
-			[[
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
-			false
-		)
-	end
+      false
+    )
+  end
 end
 
 local apply_lsp_mappings = function(bufnr)
-	local opts = { noremap = true, silent = true }
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+  local opts = { noremap = true, silent = true }
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	buf_set_keymap("v", "<leader>ca", "<cmd><C-U>lua vim.lsp.buf.range_code_action()<CR>", opts)
-	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "[d", '<cmd>lua vim.lsp.diagnostic.goto_prev({border = "rounded"})<CR>', opts)
-	buf_set_keymap("n", "]d", '<cmd>lua vim.lsp.diagnostic.goto_next({border = "rounded"})<CR>', opts)
-	buf_set_keymap("n", "gs", '<cmd>lua vim.lsp.buf.signature_help({border="rounded"})<CR>', opts)
-	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap("n", "<leader>sd", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border="rounded"})<CR>', opts)
-	buf_set_keymap("n", "<leader>dl", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-	buf_set_keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting_sync()<CR>", opts)
+  buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_set_keymap("v", "<leader>ca", "<cmd><C-U>lua vim.lsp.buf.range_code_action()<CR>", opts)
+  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<leader>sd", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  buf_set_keymap("n", "<leader>dl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  buf_set_keymap("n", "gs", '<cmd>lua vim.lsp.buf.signature_help({border="rounded"})<CR>', opts)
+  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting_sync()<CR>", opts)
 end
 
 M.common_on_attach = function(client, bufnr)
-	apply_lsp_mappings(bufnr)
-	highlight_document(client)
+  apply_lsp_mappings(bufnr)
+  highlight_document(client)
 end
 
 M.servers = {
-	"html",
-	"texlab",
-	"cssls",
-	"tailwindcss",
-	"clangd",
-	"tsserver",
-	"pyright",
-	"rust_analyzer",
-	"solargraph",
-	"r_language_server",
-	"jdtls",
-	"sumneko_lua",
+  "html",
+  "texlab",
+  "cssls",
+  "tailwindcss",
+  "clangd",
+  "tsserver",
+  "pyright",
+  "rust_analyzer",
+  "solargraph",
+  "r_language_server",
+  "jdtls",
+  "sumneko_lua",
 }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -111,7 +111,7 @@ capabilities.textDocument.formatting = false
 
 local cmp_lsp_ok, cmp_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_lsp_ok then
-	vim.notify("Failed to require cmp_nvim_lsp", vim.log.levels.WARN)
+  vim.notify("Failed to require cmp_nvim_lsp", vim.log.levels.WARN)
 end
 
 capabilities = cmp_lsp.update_capabilities(capabilities)
