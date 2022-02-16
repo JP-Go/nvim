7553d31505368e35333a9267b095e52428492a54
João Pedro <jpedro@gmail.com>
2022-02-16 13:44:11 -0300

    refactor(lsp): Use prebuilt binary for lua-language-server
    Arch now packages the sumneko_lua language server
    so I opted to use it instead of recompiling it every
    update

--- lua/lsp/settings/sumneko_lua.lua
+++ lua/lsp/settings/sumneko_lua.lua
@@ -1,11 +1,6 @@
-local sumneko_root_path = "/home/jp/Builds/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. "Linux" .. "/lua-language-server"
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
 
 return {
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+  cmd = {"lua-language-server" }
   settings = {
     Lua = {
       runtime = {
