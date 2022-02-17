4535747abfcbaaa8fb89132db406ea1672fc0f55
João Pedro <jpedro@gmail.com>
2022-02-17 17:34:28 -0300

    Remove uneeded settings

--- lua/lsp/settings/texlab.lua
+++
@@ -1,7 +0,0 @@
-local handlers = require("lsp.handlers")
-local capabilities = handlers.common_capabilities
-capabilities.textDocument.formatting = true
-
-return {
-  capabilities = capabilities,
-}
