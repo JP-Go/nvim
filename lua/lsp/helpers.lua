c7fc86fa04dfe5a7e17207ad5648aed739895b21
João Pedro <jpedro@gmail.com>
2022-05-26 10:40:11 -0300

    Change rename mapping

--- lua/lsp/helpers.lua
+++ lua/lsp/helpers.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.servers = {"pyright","tsserver","sumneko_lua"}
+M.servers = {"pyright","tsserver","sumneko_lua",}
 
 
 M.common_on_attach = function(client, bufnr)
@@ -19,14 +19,12 @@ M.common_on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   vim.keymap.set('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   vim.keymap.set('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.keymap.set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.keymap.set('n', '<space>r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.keymap.set('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.keymap.set('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 M.capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
- 
 
 return M
