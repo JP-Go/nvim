fc0900f3573b4eac370ddaf473c5236d9661fad3
João Pedro <jpedro@gmail.com>
2022-05-26 09:42:45 -0300

    Set indentation length

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -2,4 +2,7 @@ vim.opt.rnu = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.g.sonokai_style = "default"
