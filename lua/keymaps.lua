ebbe5900c9196cb61e5696e56ab3e714c57aff4c
João Pedro <jpedro@gmail.com>
2022-05-26 10:40:21 -0300

    More mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,13 @@
 local default_opts = {noremap = true, silent = true}
 
-vim.keymap.set("n","<space>sf",function ()
+local function set_keymap (mode, lhs,rhs,opts) vim.keymap.set(mode, lhs,rhs,opts) end
+
+set_keymap("n","<space>sf",function ()
 	vim.cmd(":source %")
 	print(string.format("Sourced file %s",vim.fn.expand("%")))
 end,default_opts)
-vim.keymap.set("n","<space>ff",":Telescope find_files<CR>",default_opts)
-vim.keymap.set("n","<space>b", ":Telescope buffers<CR>",default_opts)
+set_keymap("n","<space>ff",":Telescope find_files<CR>",default_opts)
+set_keymap("n","<space>b", ":Telescope buffers<CR>",default_opts)
+
+set_keymap("n","<space>e",":NvimTreeToggle<CR>",default_opts)
+
