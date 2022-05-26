687798d0f14358b00449bd1785c41729dc21a4e4
João Pedro <jpedro@gmail.com>
2022-05-26 09:42:06 -0300

    Create new keymaps to move arround easily and source files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,8 @@
-vim.keymap.set("n","<space>ff",":Telescope find_files<CR>")
+local default_opts = {noremap = true, silent = true}
+
+vim.keymap.set("n","<space>sf",function ()
+	vim.cmd(":source %")
+	print(string.format("Sourced file %s",vim.fn.expand("%")))
+end,default_opts)
+vim.keymap.set("n","<space>ff",":Telescope find_files<CR>",default_opts)
+vim.keymap.set("n","<space>b", ":Telescope buffers<CR>",default_opts)
