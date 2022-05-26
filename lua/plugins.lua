825e3efb807447e5740455850ab9eb51c2e58cc9
João Pedro <jpedro@gmail.com>
2022-05-26 10:40:48 -0300

    Add and configure nvim-tree plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,7 +10,15 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'navarasu/onedark.nvim'
-
+	use {
+			'kyazdani42/nvim-tree.lua',
+			requires = {
+				'kyazdani42/nvim-web-devicons', -- optional, for file icon
+			},
+			config = function ()
+				require("nvim-tree-config")
+			end
+	}
   use 'L3MON4D3/LuaSnip'
   use 'rafamadriz/friendly-snippets'
   use {'hrsh7th/nvim-cmp', config = function() require("cmp-config") end}
