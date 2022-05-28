0489ff0e9f13a69c4f8f66ec7747dd59be58073a
João Pedro <jpedro@gmail.com>
2022-05-28 18:29:22 -0300

    refactor: Pass functions to the rhs of the keymap.set function instead of strings

--- lua/lsp/helpers.lua
+++ lua/lsp/helpers.lua
@@ -1,31 +1,32 @@
 local M = {}
 
-M.servers = {"pyright","tsserver","sumneko_lua",}
+M.servers = { "pyright", "tsserver", "sumneko_lua", }
 
+---@diagnostic disable-next-line: unused-local
 M.common_on_attach = function(client, bufnr)
-	vim.diagnostic.config({virtual_text = false})
-  local opts = {buffer = true, noremap = true}
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.diagnostic.config({ virtual_text = false })
+	local opts = { buffer = true, noremap = true }
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.keymap.set('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.keymap.set('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)
-  vim.keymap.set('n', 'K', ':lua vim.lsp.buf.hover()<CR>', opts)
-  vim.keymap.set('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.keymap.set('n', '<C-k>', ':lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.keymap.set('n', '<space>wa', ':lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.keymap.set('n', '<space>wr', ':lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.keymap.set('n', '<space>wl', ':lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.keymap.set('n', '<space>D', ':lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.keymap.set('n', '<space>r', ':lua vim.lsp.buf.rename()<CR>', opts)
-  vim.keymap.set('n', '<space>ca', ':lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.keymap.set('n', 'gr', ':lua vim.lsp.buf.references()<CR>', opts)
-  vim.keymap.set('n', '<space>f', ':lua vim.lsp.buf.formatting()<CR>', opts)
-  vim.keymap.set('n', '<space>d', ':lua vim.diagnostic.open_float()<CR>', opts)
-  vim.keymap.set('n', ']d', ':lua vim.diagnostic.goto_next()<CR>', opts)
-  vim.keymap.set('n', '[d', ':lua vim.diagnostic.goto_prev()<CR>', opts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+	vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+	vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+	vim.keymap.set('n', '<space>fc', vim.lsp.buf.formatting, opts)
+	vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
