21c1e92f88ab6aecee35c681fd8342c96b006ec6
João Pedro <jpedro@gmail.com>
2022-05-28 02:37:01 -0300

    Use the binary in the default folders

--- lua/lsp/configs/sumneko_lua.lua
+++ lua/lsp/configs/sumneko_lua.lua
@@ -2,7 +2,7 @@ local on_attach = require("lsp.helpers").common_on_attach
 local capabilities = require("lsp.helpers").capabilities
 
 return {
-	cmd = {"/home/jp/.local/bin/lua-language-server/bin/lua-language-server"},
+	cmd = {"lua-language-server"},
 	on_attach = on_attach,
 	capabilities = capabilities,
 }
