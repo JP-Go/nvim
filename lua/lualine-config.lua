a408a1f7faa05e4120e97a993472df71c68d6919
João Pedro <jpedro@gmail.com>
2022-05-28 18:13:44 -0300

    Add and configure lualine

---
+++ lua/lualine-config.lua
@@ -0,0 +1,35 @@
+-- Eviline config for lualine
+-- Author: shadmansaleh
+-- Credit: glepnir
+local lualine = require('lualine')
+local config =  { 
+  options = {
+    icons_enabled = true,
+		theme = "onedark",
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = {'encoding'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+lualine.setup(config)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,38 +1,43 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
+	use 'wbthomason/packer.nvim'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
 		require("treesitter-config")
-  end}
-  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
+	end }
+	use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
 	use 'jose-elias-alvarez/null-ls.nvim'
-  use 'navarasu/onedark.nvim'
+	use 'navarasu/onedark.nvim'
 	use {
-			'kyazdani42/nvim-tree.lua',
-			requires = {
-				'kyazdani42/nvim-web-devicons', -- optional, for file icon
-			},
-			config = function ()
-				require("nvim-tree-config")
-			end
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+		},
+		config = function()
+			require("nvim-tree-config")
+		end
 	}
-  use 'L3MON4D3/LuaSnip'
-  use 'rafamadriz/friendly-snippets'
-  use {'hrsh7th/nvim-cmp', config = function() require("cmp-config") end}
-  use 'sainnhe/sonokai'
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		config = function()
+			require("lualine-config")
+		end
+	}
+	use 'L3MON4D3/LuaSnip'
+	use 'rafamadriz/friendly-snippets'
+	use { 'hrsh7th/nvim-cmp', config = function() require("cmp-config") end }
+	use 'sainnhe/sonokai'
 	use 'TimUntersberger/neogit'
 
-		if packer_bootstrap then
-			require('packer').sync()
-		end
-  end)
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
