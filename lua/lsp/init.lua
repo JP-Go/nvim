dadb4b2dd6ab3539aff34123329ac8105be81559
João Pedro <jpedro@gmail.com>
2022-05-28 20:48:54 -0300

    Use only null-ls to format buffers

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,35 +1,41 @@
-local lspconfig_ok, lspconfig = pcall(require,"lspconfig")
-local null_ls_ok,null_ls = pcall(require,"null-ls")
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
 
 local servers = require("lsp.helpers").servers
 local on_attach = require("lsp.helpers").common_on_attach
 local capabilities = require("lsp.helpers").capabilities
 
 if not lspconfig_ok then
- 	print("lspconfig not installed")
+	print("lspconfig not installed")
 	return
 end
 
-for _,server in pairs(servers) do
-	local has_special_config,config = pcall(require,string.format("lsp.configs.%s", server))
-	if has_special_config then
-		lspconfig[server].setup(config)
-	else
-		lspconfig[server].setup( {
-			on_attach = on_attach,
-			capabilities = capabilities
-		})
-	end
+for _, server in pairs(servers) do
+	lspconfig[server].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-if not null_ls_ok then
- 	print("null_ls not installed")
-	return
-end
+local null_ls = require("null-ls")
 
 local formatters = null_ls.builtins.formatting
 
 null_ls.setup({
-	formatters.stylua,
-	formatters.prettierd
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.keymap.set("n", "<space>cf", function()
+						vim.lsp.buf.formatting_seq_sync({}, 2000, { "null_ls" })
+					end)
+					vim.lsp.buf.formatting_seq_sync({}, 2000, { "null_ls" })
+				end,
+			})
+		end
+	end,
+	sources = { formatters.prettierd, formatters.stylua },
 })
