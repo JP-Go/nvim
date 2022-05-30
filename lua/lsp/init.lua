206a91ee6cae01a1502d430f48ba51f54ba03305
João Pedro <jpedro@gmail.com>
2022-05-29 23:39:17 -0300

    Extern configuration of null-ls to its own file Get the configuration module from the config_module field of each server

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -10,32 +10,10 @@ if not lspconfig_ok then
 end
 
 for _, server in pairs(servers) do
-	lspconfig[server].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+	local options = { on_attach = on_attach, capabilities = capabilities }
+	if server.config_module ~= nil then
+		local server_config = require(server.config_module)
+		options = vim.tbl_extend("force", options, server_config)
+	end
+	lspconfig[server.name].setup(options)
 end
-
-local null_ls = require("null-ls")
-
-local formatters = null_ls.builtins.formatting
-
-null_ls.setup({
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.keymap.set("n", "<space>cf", function()
-						vim.lsp.buf.formatting_seq_sync({}, 2000, { "null_ls" })
-					end)
-					vim.lsp.buf.formatting_seq_sync({}, 2000, { "null_ls" })
-				end,
-			})
-		end
-	end,
-	sources = { formatters.prettierd, formatters.stylua },
-})
---
+++ lua/null-ls-config.lua
@@ -0,0 +1,27 @@
+local nls_ok, nls = pcall(require, 'null-ls')
+local util = require('vim.lsp.util')
+
+if not nls_ok then
+	print('Null-ls not installed')
+	return
+end
+
+local formatters = nls.builtins.formatting
+local formatting_callback = function(client, bufnr)
+	vim.keymap.set('n', '<space>cf', function()
+		local params = util.make_formatting_params({})
+		client.request('textDocument/formatting', params, nil, bufnr)
+	end, { buffer = bufnr })
+end
+
+nls.setup({
+	on_attach = function(client, bufnr)
+		formatting_callback(client, bufnr)
+	end,
+	sources = {
+		formatters.stylua.with({
+			extra_args = { '--config-path=/home/jp/.config/stylua.toml' },
+		}),
+		formatters.prettier,
+	},
+})
