b512c36aaf23cfd61a1849fd5657dc13d4cba7d6
João Pedro <jpedro@gmail.com>
2022-05-29 23:33:34 -0300

    Formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,52 +1,78 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
-		require("treesitter-config")
-	end }
-	use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'jose-elias-alvarez/null-ls.nvim'
-	use 'navarasu/onedark.nvim'
-	use { 'lewis6991/gitsigns.nvim', tag = 'release', config = function()
-		require('gitsigns').setup({ current_line_blame = true,
-			current_line_blame_opts = {
-				virt_text = true,
-				virt_text_pos = 'right_align',
-				delay = 1000,
-				ignore_whitespace = false,
-			} })
-	end }
-	use {
-		'kyazdani42/nvim-tree.lua',
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("treesitter-config")
+		end,
+	})
+	use({ "jose-elias-alvarez/null-ls.nvim" , config = function ()
+		require("null-ls-config")
+	end})
+	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("navarasu/onedark.nvim")
+	use({
+		"lewis6991/gitsigns.nvim",
+		tag = "release",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "right_align",
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+			})
+		end,
+	})
+	use({
+		"kyazdani42/nvim-tree.lua",
 		requires = {
-			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
 		},
 		config = function()
 			require("nvim-tree-config")
-		end
-	}
-	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		end,
+	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 		config = function()
 			require("lualine-config")
-		end
-	}
-	use 'L3MON4D3/LuaSnip'
-	use 'rafamadriz/friendly-snippets'
-	use { 'hrsh7th/nvim-cmp', config = function() require("cmp-config") end }
-	use 'sainnhe/sonokai'
-	use 'TimUntersberger/neogit'
+		end,
+	})
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
+	use({
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("cmp-config")
+		end,
+	})
+	use("sainnhe/sonokai")
+	use("TimUntersberger/neogit")
 
 	if packer_bootstrap then
-		require('packer').sync()
+		require("packer").sync()
 	end
 end)
