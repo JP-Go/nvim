38b8684ce4d944df11e2de6445815e2fc3c77c3f
João Pedro <jpedro@gmail.com>
2022-07-05 15:52:31 -0300

    Formatting

--- lua/cmp-config.lua
+++ lua/cmp-config.lua
@@ -1,30 +1,34 @@
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	return col ~= 0
+		and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+				:sub(col, col)
+				:match('%s')
+			== nil
 end
-local cmp_ok, cmp = pcall(require, "cmp")
-local luasnip_ok, luasnip = pcall(require, "luasnip")
+local cmp_ok, cmp = pcall(require, 'cmp')
+local luasnip_ok, luasnip = pcall(require, 'luasnip')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-require("luasnip.loaders.from_vscode").lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load()
 
 if not cmp_ok then
-	print("cmp not installed")
+	print('cmp not installed')
 	return
 end
 
 if not luasnip_ok then
-	print("luasnip not installed")
+	print('luasnip not installed')
 	return
 end
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping(function(fallback)
+		['<Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
@@ -34,9 +38,9 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
+		end, { 'i', 's' }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		['<S-Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -44,45 +48,45 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
+		end, { 'i', 's' }),
 
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
-	sources = cmp.config.sources({ { name = "nvim_lsp", max_item_count = 30 }, { name = "luasnip" } }, {
-		{ name = "buffer", keyword_length = 5 },
-	}),
+	sources = cmp.config.sources(
+		{ { name = 'nvim_lsp', max_item_count = 30 }, { name = 'luasnip' } },
+		{
+			{ name = 'buffer', keyword_length = 5 },
+		}
+	),
 })
-cmp.setup.filetype("gitcommit", {
+cmp.setup.filetype('gitcommit', {
 	sources = cmp.config.sources({
-		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
 	}, {
-		{ name = "buffer" },
+		{ name = 'buffer' },
 	}),
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
+cmp.setup.cmdline('/', {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = "buffer" },
+		{ name = 'buffer' },
 	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
+cmp.setup.cmdline(':', {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = "path" },
+		{ name = 'path' },
 	}, {
-		{ name = "cmdline" },
+		{ name = 'cmdline' },
 	}),
 })
 
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
