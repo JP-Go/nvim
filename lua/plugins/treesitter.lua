381eb24e0e33505ea754fa5b07bd6baff9256afb
João Pedro <jpedro1580@gmail.com>
2023-04-24 20:38:50 -0300

    Configure plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      -- General
+      "python",
+
+      -- Webdev
+      "tsx",
+      "typescript",
+      "javascript",
+      "css",
+      "vue",
+      "html",
+    },
+  },
+}
