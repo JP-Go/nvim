d78a0f7f546125d2a3bebd7bf31c2e3e26e0e64d
João Pedro <jpedro1580@gmail.com>
2023-04-24 20:41:54 -0300

    Configure mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "css-ls",
+      "docker-compose-language-service docker_compose_language_service",
+      "dockerfile-language-server dockerls",
+      "json-lsp jsonls",
+      "lua-language-server lua_ls",
+      "pyright",
+      "shfmt",
+      "stylua",
+      "typescript-language-server tsserver",
+      "unocss-language-server unocss",
+      "vue-language-server volar",
+    },
+  },
+}
