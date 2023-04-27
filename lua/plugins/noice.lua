e41c8f5b3ced45671ec13d2b197cf998c130a1d4
João Pedro <jpedro1580@gmail.com>
2023-04-27 19:57:07 -0300

    Remove some ui eye candy

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,5 +8,16 @@ return {
       inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+    },
+    health = {
+      checker = false,
+    },
+    notify = {
+      enabled = false,
+    },
   },
 }
