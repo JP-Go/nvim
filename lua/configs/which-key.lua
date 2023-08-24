54f15bdcadcafb7f2d699d2e97232722a82b6bdb
João Pedro <jpedro1580@gmail.com>
2023-08-24 19:50:50 -0300

    Add buffer manipulation mappings

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -8,6 +8,12 @@ wk.register({
 		h = {"<cmd>Telescope help_tags<cr>", "Find gelp"},
 		b = {"<cmd>Telescope buffers<cr>", "Find buffers"},
 	},
+    b = {
+        n = {"<cmd>bNext<cr>","Next buffer"},
+        p = {"<cmd>bprev<cr>","Previous buffer"},
+        c = {"<cmd>bdelete<cr>","Delete buffer"},
+        C = {"<cmd>bdelete!<cr>","Delete buffer (force)"},
+    },
     e = {"<cmd>NvimTreeToggle<cr>", "Toggle file tree"},
     ["ss"] = {"<cmd>so%<cr>","Source current file"},
 }, {prefix = "<leader>"})
