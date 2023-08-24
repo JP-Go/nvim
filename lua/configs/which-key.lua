3c30baa9f441bd574ce47fe267fe2d1b9fc9d712
João Pedro <jpedro1580@gmail.com>
2023-08-24 17:27:52 -0300

    Change case for some words in the description of mappings

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -3,11 +3,11 @@ local wk = require("which-key")
 wk.register({
 	f = {
 		name = "Find",
-		f = {"<cmd>Telescope find_files<cr>", "Find Files"},
-		w = {"<cmd>Telescope live_grep<cr>", "Find Word"},
-		h = {"<cmd>Telescope help_tags<cr>", "Find Help"},
+		f = {"<cmd>Telescope find_files<cr>", "Find files"},
+		w = {"<cmd>Telescope live_grep<cr>", "Find word"},
+		h = {"<cmd>Telescope help_tags<cr>", "Find gelp"},
+		b = {"<cmd>Telescope buffers<cr>", "Find buffers"},
 	},
-    e = {"<cmd>NvimTreeToggle<cr>", "Toggle File-tree"},
-    ["ss"] = {"<cmd>so%<cr>","Source file"},
+    e = {"<cmd>NvimTreeToggle<cr>", "Toggle file tree"},
+    ["ss"] = {"<cmd>so%<cr>","Source current file"},
 }, {prefix = "<leader>"})
-
