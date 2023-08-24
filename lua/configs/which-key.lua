aca6a7366e5309ab778c13f124754a54776bda4e
João Pedro <jpedro1580@gmail.com>
2023-08-24 17:19:02 -0300

    Add some convenience keymaps for sourcing files and nvim-tree interaction

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -5,5 +5,9 @@ wk.register({
 		name = "Find",
 		f = {"<cmd>Telescope find_files<cr>", "Find Files"},
 		w = {"<cmd>Telescope live_grep<cr>", "Find Word"},
-	}
+		h = {"<cmd>Telescope help_tags<cr>", "Find Help"},
+	},
+    e = {"<cmd>NvimTreeToggle<cr>", "Toggle File-tree"},
+    ["ss"] = {"<cmd>so%<cr>","Source file"},
 }, {prefix = "<leader>"})
+
