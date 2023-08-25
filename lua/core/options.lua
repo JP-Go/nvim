eefb9044011a447cc2a1be32a7ab4a4c5f942fa3
João Pedro <jpedro1580@gmail.com>
2023-08-25 14:31:24 -0300

    Remove duplicated fields

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,10 +6,13 @@ local opts = {
 	splitright = true,
 	softtabstop = 4,
 	tabstop = 4,
-	expandtab = true,
 	shiftwidth = 4,
 	shiftround = true,
 	expandtab = true,
+    cmdheight = 2,
+    textwidth = 80,
+    number = true
+
 }
 
 for k,v in pairs(opts) do
