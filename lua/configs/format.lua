3b66608faa979e85a616c41cbce2c0cc783765b7
João Pedro <jpedro1580@gmail.com>
2023-08-28 17:00:53 -0300

    Add formatter.nvim

---
+++ lua/configs/format.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.opts = {
+  filetype = {
+    lua = { require('formatter.filetypes.lua').stylua },
+    javascript = { require('formatter.filetypes.javascript').denofmt },
+    typescript = { require('formatter.filetypes.typescript').denofmt },
+    javascriptreact = { require('formatter.filetypes.javascriptreact').denofmt },
+    typescriptreact = { require('formatter.filetypes.typescriptreact').denofmt },
+  },
+}
+
+return M
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,116 +1,138 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-			require("configs.which-key")
-		end,
-	},
-    {
-        'stevearc/dressing.nvim',
-        opts = {},
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require('configs.which-key')
+    end,
+  },
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
     },
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
+    cmd = {
+      'NvimTreeToggle',
+      'NvimTreeFocus',
+      'NvimTreeFindFile',
+      'NvimTreeCollapse',
+    },
+    opts = require('configs.nvim-tree').opts,
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {},
+  },
+  {
+    'romgrk/barbar.nvim',
+    config = function()
+      require('configs.barbar')
+    end,
+  },
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      require('onedark').setup({
+        style = 'darker',
+      })
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = { 'Telescope' },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('configs.treesitter')
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('configs.lspconfig')
+      require('mason').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'lua_ls',
+          'clangd',
+          'tsserver',
+          'cssls',
+          'tailwindcss',
+          'prismals',
+          'dockerls',
+          'docker_compose_language_service',
         },
-        cmd = {"NvimTreeToggle","NvimTreeFocus","NvimTreeFindFile","NvimTreeCollapse"},
-        opts = require("configs.nvim-tree").opts
+      })
+    end,
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
     },
-    {
-        'lewis6991/gitsigns.nvim',
-        opts = {}
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'windwp/nvim-autopairs',
     },
-    {
-        "romgrk/barbar.nvim",
-        config = function ()
-            require('configs.barbar')
-        end
+    config = function()
+      require('cmp').setup(require('configs.cmp').opts)
+    end,
+    event = 'InsertEnter',
+  },
+  {
+    'TimUntersberger/neogit',
+    config = function(_, opts)
+      require('neogit').setup(opts)
+      require('configs.git')
+    end,
+    opts = {},
+  },
+  {
+    'smoka7/multicursors.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'smoka7/hydra.nvim',
     },
-    {
-        "navarasu/onedark.nvim",
-        priority = 1000,
-        config = function ()
-            require("onedark").setup({
-                style = 'darker'
-            })
-        end
+    opts = {},
+    cmd = {
+      'MCstart',
+      'MCvisual',
+      'MCclear',
+      'MCpattern',
+      'MCvisualPattern',
+      'MCunderCursor',
     },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		dependencies = {"nvim-lua/plenary.nvim"},
-        cmd = {"Telescope"}
-	},
-	{	"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("configs.treesitter")
-		end,
-	},
-	{
-		'williamboman/mason.nvim',
-		config = function()
-            require("configs.lspconfig")
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls","clangd","tsserver","cssls","tailwindcss","prismals", "dockerls","docker_compose_language_service" }
-			})
-		end,
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-
-		}
-
-	},
-    {
-         'hrsh7th/nvim-cmp',
-         dependencies = {
-             'hrsh7th/cmp-nvim-lsp',
-             'hrsh7th/cmp-buffer',
-             'hrsh7th/cmp-path',
-             'hrsh7th/cmp-cmdline',
-             'L3MON4D3/LuaSnip',
-             'saadparwaiz1/cmp_luasnip',
-             'windwp/nvim-autopairs'
-         },
-         config = function()
-             require('cmp').setup(
-                require("configs.cmp").opts
-             )
-         end,
-         event = "InsertEnter"
-     },
-     {
-         "TimUntersberger/neogit",
-         config = function (_,opts)
-            require("neogit").setup(opts)
-            require('configs.git')
-         end,
-         opts = {},
-     },
-     -- lazy.nvim:
-     {
-         "smoka7/multicursors.nvim",
-         event = "VeryLazy",
-         dependencies = {
-             'nvim-treesitter/nvim-treesitter',
-             'smoka7/hydra.nvim',
-         },
-         opts = {},
-         cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-         keys = {
-             {
-                 mode = { 'v', 'n' },
-                 '<C-n>',
-                 '<cmd>MCstart<cr>',
-                 desc = 'Create a selection for selected text or word under the cursor',
-             },
-         },
-     }
-
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<C-n>',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
+  {
+    'mhartington/formatter.nvim',
+    config = function()
+      require('formatter').setup(require('configs.format').opts)
+    end,
+  },
 }
