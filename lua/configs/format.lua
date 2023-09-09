ae87dd92161015c3c76b6e7bed71c4dc77e60814
João Pedro <jpedro1580@gmail.com>
2023-09-09 11:50:44 -0300

    Add sql formatter

--- lua/configs/format.lua
+++ lua/configs/format.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 M.opts = {
-  filetype = {
-    lua = { require('formatter.filetypes.lua').stylua },
-    javascript = { require('formatter.filetypes.javascript').denofmt },
-    typescript = { require('formatter.filetypes.typescript').denofmt },
-    javascriptreact = { require('formatter.filetypes.javascriptreact').denofmt },
-    typescriptreact = { require('formatter.filetypes.typescriptreact').denofmt },
-    vue = { require('formatter.filetypes.vue').prettier },
-  },
+	filetype = {
+		lua = { require("formatter.filetypes.lua").stylua },
+		javascript = { require("formatter.filetypes.javascript").denofmt },
+		typescript = { require("formatter.filetypes.typescript").denofmt },
+		javascriptreact = { require("formatter.filetypes.javascriptreact").denofmt },
+		typescriptreact = { require("formatter.filetypes.typescriptreact").denofmt },
+		vue = { require("formatter.filetypes.vue").prettier },
+		sql = { require("formatter.filetypes.sql").pg_format },
+	},
 }
 
 return M
