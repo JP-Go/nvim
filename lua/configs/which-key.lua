306f10aca95eeae6c24b1c0613d619fd127b6898
João Pedro <jpedro1580@gmail.com>
2023-09-13 20:41:23 -0300

    Mapping to clear search highlight

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -1,19 +1,22 @@
-local wk = require("which-key")
+local wk = require('which-key')
 
 wk.register({
-	f = {
-		name = "Find",
-		f = {"<cmd>Telescope find_files<cr>", "Find files"},
-		w = {"<cmd>Telescope live_grep<cr>", "Find word"},
-		h = {"<cmd>Telescope help_tags<cr>", "Find gelp"},
-		b = {"<cmd>Telescope buffers<cr>", "Find buffers"},
-	},
-    b = {
-        n = {"<cmd>bNext<cr>","Next buffer"},
-        b = {"<cmd>bprev<cr>","Previous buffer"},
-        c = {"<cmd>bdelete<cr>","Delete buffer"},
-        C = {"<cmd>bdelete!<cr>","Delete buffer (force)"},
-    },
-    e = {"<cmd>NvimTreeToggle<cr>", "Toggle file tree"},
-    ["ss"] = {"<cmd>so%<cr>","Source current file"},
-}, {prefix = "<leader>"})
+  f = {
+    name = 'Find',
+    f = { '<cmd>Telescope find_files<cr>', 'Find files' },
+    w = { '<cmd>Telescope live_grep<cr>', 'Find word' },
+    h = { '<cmd>Telescope help_tags<cr>', 'Find gelp' },
+    b = { '<cmd>Telescope buffers<cr>', 'Find buffers' },
+  },
+  b = {
+    n = { '<cmd>bNext<cr>', 'Next buffer' },
+    b = { '<cmd>bprev<cr>', 'Previous buffer' },
+    c = { '<cmd>bdelete<cr>', 'Delete buffer' },
+    C = { '<cmd>bdelete!<cr>', 'Delete buffer (force)' },
+  },
+  e = { '<cmd>NvimTreeToggle<cr>', 'Toggle file tree' },
+  ['ss'] = { '<cmd>so%<cr>', 'Source current file' },
+}, { prefix = '<leader>' })
+wk.register({
+  ['<ESC>'] = { '<cmd>nohlsearch<cr>', 'Clear search highlight' },
+})
