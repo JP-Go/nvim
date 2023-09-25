980a6e97be53aa7e6dbf932ad657d9a3d669a91d
João Pedro <jpedro1580@gmail.com>
2023-09-25 02:29:22 -0300

    Add clang format for some c-like languages

--- lua/configs/format.lua
+++ lua/configs/format.lua
@@ -10,6 +10,9 @@ M.opts = {
     vue = { require('formatter.filetypes.vue').prettier },
     sql = { require('formatter.filetypes.sql').pg_format },
     go = { require('formatter.filetypes.go').gofmt },
+    cs = { require('formatter.filetypes.cs').clangformat },
+    c = { require('formatter.filetypes.c').clangformat },
+    java = { require('formatter.filetypes.java').clangformat },
   },
 }
 
