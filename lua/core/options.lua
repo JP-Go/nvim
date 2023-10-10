f37b9ebe47f8e661005950c49070e5e0dfbab63b
João Pedro <jpedro1580@gmail.com>
2023-10-10 11:53:22 -0300

    feat: add hidden option

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,20 +1,20 @@
 local opts = {
-	termguicolors = true,
-	backup = false,
-	swapfile = false,
-	undofile = true,
-	splitright = true,
-	softtabstop = 4,
-	tabstop = 4,
-	shiftwidth = 4,
-	shiftround = true,
-	expandtab = true,
-    cmdheight = 2,
-    textwidth = 80,
-    number = true
-
+  termguicolors = true,
+  backup = false,
+  swapfile = false,
+  undofile = true,
+  splitright = true,
+  softtabstop = 4,
+  tabstop = 4,
+  shiftwidth = 4,
+  shiftround = true,
+  expandtab = true,
+  cmdheight = 2,
+  textwidth = 80,
+  number = true,
+  hidden = true,
 }
 
-for k,v in pairs(opts) do
-	vim.opt[k] = v
+for k, v in pairs(opts) do
+  vim.opt[k] = v
 end
