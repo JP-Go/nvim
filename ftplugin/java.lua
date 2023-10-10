9ef3bb7b0a485ecfeef5a76a495a7121e6bee30c
João Pedro <jpedro1580@gmail.com>
2023-10-10 20:17:30 -0300

    Configure jdtls for java

---
+++ ftplugin/java.lua
@@ -0,0 +1,75 @@
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local jdtls_install_location = vim.fn.expand('$HOME')
+  .. '/.local/share/nvim/mason/packages/jdtls'
+local launcher_path = jdtls_install_location
+  .. '/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar'
+local config_path = jdtls_install_location .. '/config_linux'
+local workspace_dir = vim.fn.expand('$HOME')
+  .. '/.java/workspaces/'
+  .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local get_java_agent = function(ls_loc)
+  local agent = ''
+  local expected_lombok_location = ls_loc .. '/lombok.jar'
+  if vim.fn.exists(expected_lombok_location) then
+    agent = '-javaagent:' .. expected_lombok_location
+  end
+  return agent
+end
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    -- 💀
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    get_java_agent(jdtls_install_location),
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar',
+    launcher_path,
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    -- Must point to the
+    -- eclipse.jdt.ls installation
+    '-configuration',
+    config_path,
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data',
+    workspace_dir,
+  },
+
+  settings = {
+    java = {},
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {
+      jdtls_install_location .. '/lombok.jar',
+    },
+  },
+}
+require('jdtls').start_or_attach(config)
