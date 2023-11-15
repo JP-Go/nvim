654a6c317ec632734a885c2553b65e506ecb522b
João Pedro <jpedro1580@gmail.com>
2023-11-15 11:05:44 -0300

    Remove multicursors (Its awkward to use)

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -117,31 +117,6 @@ return {
     opts = {},
   },
   {
-    'smoka7/multicursors.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'smoka7/hydra.nvim',
-    },
-    opts = {},
-    cmd = {
-      'MCstart',
-      'MCvisual',
-      'MCclear',
-      'MCpattern',
-      'MCvisualPattern',
-      'MCunderCursor',
-    },
-    keys = {
-      {
-        mode = { 'v', 'n' },
-        '<C-n>',
-        '<cmd>MCstart<cr>',
-        desc = 'Create a selection for selected text or word under the cursor',
-      },
-    },
-  },
-  {
     'mhartington/formatter.nvim',
     config = function()
       require('formatter').setup(require('configs.format').opts)
