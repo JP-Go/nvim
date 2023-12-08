a4bb93832303f5642a096f2210742f0ec8c2ac63
João Pedro <jpedro1580@gmail.com>
2023-12-08 19:30:55 -0300

    feat: Add keybindings for branches and deleted neogit

--- lua/configs/git.lua
+++ lua/configs/git.lua
@@ -1,9 +1,8 @@
 local wk = require('which-key')
 wk.register({
   g = {
-    g = { '<cmd>Neogit kind=vsplit<cr>', 'Open neogit' },
-    c = { '<cmd>Neogit commit<cr>', 'Open new commit' },
     s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage current hunk' },
     S = { '<cmd>Telescope git_status<cr>', 'Find git changes' },
+    b = { '<cmd>Telescope git_branches<cr>', 'List git branches' },
   },
 }, { prefix = '<leader>', noremap = true, silent = true })
