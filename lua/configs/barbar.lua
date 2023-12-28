629a3940e6f15ee26ec634330eb7c2eee6d5f273
João Pedro <jpedro1580@gmail.com>
2023-12-27 22:10:30 -0300

    feat: Remove needless configs

--- lua/configs/barbar.lua
+++
@@ -1,8 +0,0 @@
-local wk = require('which-key')
-wk.register({
-    b = {
-        c = {'<cmd>BufferClose<cr>', 'Close buffer'},
-        n = {'<cmd>BufferNext<cr>', 'Next buffer'},
-        b = {'<cmd>BufferPrevious<cr>', 'Previous buffer'},
-    }
-},{prefix = '<leader>', noremap = true, silent = true})
--- lua/configs/git.lua
+++
@@ -1,8 +0,0 @@
-local wk = require('which-key')
-wk.register({
-  g = {
-    s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage current hunk' },
-    S = { '<cmd>Telescope git_status<cr>', 'Find git changes' },
-    b = { '<cmd>Telescope git_branches<cr>', 'List git branches' },
-  },
-}, { prefix = '<leader>', noremap = true, silent = true })
