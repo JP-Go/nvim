c13581a0c81098e753fbfedb06c3f6c66a3cfe31
João Pedro <jpedro1580@gmail.com>
2023-12-27 22:06:45 -0300

    feat: restructure plugin system
    This moves plugin declaration and configuration to specified files, grouping them
    into modules when the functionality of the plugins grouped is similar

---
+++ lua/plugins/text-manipulation.lua
@@ -0,0 +1,11 @@
+return {
+  { 'echasnovski/mini.surround', version = '*', config = true },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    opts = {},
+  },
+  {
+    'Vonr/align.nvim',
+  },
+}
