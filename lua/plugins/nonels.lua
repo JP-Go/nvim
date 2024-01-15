0e97cbdaa8fae70ebc9fb74d6ef5217e0f46ddc5
João Pedro <jpedro1580@gmail.com>
2024-01-15 20:51:18 -0300

    feat: use default eslint and prettier (running as a daemon is not great)

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,55 +1,53 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint_d.with({
-					condition = function(utils)
-						return not utils.root_has_file({
-							"biome.json",
-						}) and utils.root_has_file({
-							"eslint.config.js",
-							".eslintrc",
-							".eslintrc.js",
-							".eslintrc.cjs",
-							".eslintrc.yaml",
-							".eslintrc.yml",
-							".eslintrc.json",
-						})
-					end,
-				}),
-				null_ls.builtins.formatting.prettierd.with({
-					condition = function(utils)
-						return not utils.root_has_file({
-							"biome.json",
-						})
-					end,
-				}),
-				null_ls.builtins.formatting.biome.with({
-					condition = function(utils)
-						return utils.root_has_file({
-							"biome.json",
-						})
-					end,
-				}),
-			},
-		})
-	end,
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require('null-ls')
+    null_ls.setup({
+      on_attach = function(client, bufnr)
+        if client.supports_method('textDocument/formatting') then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.eslint.with({
+          condition = function(utils)
+            return utils.root_has_file({
+              'eslint.config.js',
+              '.eslintrc',
+              '.eslintrc.js',
+              '.eslintrc.cjs',
+              '.eslintrc.yaml',
+              '.eslintrc.yml',
+              '.eslintrc.json',
+            })
+          end,
+        }),
+        null_ls.builtins.formatting.prettier.with({
+          condition = function(utils)
+            return not utils.root_has_file({
+              'biome.json',
+            })
+          end,
+        }),
+        null_ls.builtins.formatting.biome.with({
+          condition = function(utils)
+            return utils.root_has_file({
+              'biome.json',
+            })
+          end,
+        }),
+      },
+    })
+  end,
 }
