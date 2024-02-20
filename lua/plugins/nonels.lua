dc7f43d35c558133dc01ce6ee2c3c44528200cfc
João Pedro <jpedro1580@gmail.com>
2024-02-20 19:26:57 -0300

    feat: adds keymaps to format code and execute code actions on ranges

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,12 +1,36 @@
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 return {
   'nvimtools/none-ls.nvim',
+  dependencies = {
+    'folke/which-key.nvim',
+  },
+  event = 'VeryLazy',
   config = function()
     local null_ls = require('null-ls')
     null_ls.setup({
       on_attach = function(client, bufnr)
         if client.supports_method('textDocument/formatting') then
           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+
+          local wk = require('which-key')
+          wk.register({
+            ['<leader>'] = {
+              ['cf'] = {
+                function()
+                  vim.lsp.buf.format({ async = false })
+                end,
+                'Format buffer',
+              },
+            },
+          })
+          wk.register({
+            ['<leader>'] = {
+              ['cf'] = {
+                vim.lsp.buf.format({ async = false }),
+                'Format the current range (if supported)',
+              },
+            },
+          }, { mode = 'x' })
           vim.api.nvim_create_autocmd('BufWritePre', {
             group = augroup,
             buffer = bufnr,
@@ -19,6 +43,7 @@ return {
         end
       end,
       sources = {
+        null_ls.builtins.formatting.rustfmt,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint.with({
           condition = function(utils)
