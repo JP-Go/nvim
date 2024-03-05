a2b78384a94f754626f9fae4568b704ae236d621
João Pedro <jpedro1580@gmail.com>
2024-03-05 19:42:51 -0300

    feat: Replace nonels with conform

--- lua/plugins/text-manipulation.lua
+++ lua/plugins/text-manipulation.lua
@@ -1,11 +1,43 @@
 return {
-	{ "echasnovski/mini.surround", version = "*", config = true },
-	{
-		"numToStr/Comment.nvim",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"Vonr/align.nvim",
-	},
+  { 'echasnovski/mini.surround', version = '*', config = true },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        '<leader>cf',
+        function()
+          require('conform').format({ async = true, lsp_fallback = true })
+        end,
+        mode = 'n',
+        desc = 'Format buffer',
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        go = { 'gofmt' },
+        sh = { 'shfmt' },
+        rust = { 'rustfmt' },
+        lua = { 'stylua' },
+        javascript = { { 'prettierd', 'prettier' } },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    opts = {},
+  },
+  {
+    'Vonr/align.nvim',
+  },
 }
