af53ec496699e6b26fb8cfb7fd59f635606a8475
João Pedro <jpedro1580@gmail.com>
2024-05-07 14:06:57 -0300

    feat: adds neogit client

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,4 +3,21 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = true,
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+		keys = {
+			{
+				"<leader>gg",
+				"<cmd>Neogit<cr>",
+				mode = "n",
+				desc = "Open git panel",
+			},
+		},
+	},
 }
