fc27b0c3d3f7d5065ab11ba3647ea452c2c9a2f1
João Pedro <jpedro1580@gmail.com>
2024-05-21 16:01:12 -0300

    feat: Adds fzf native to the config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,17 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
 	cmd = { "Telescope" },
 	config = function()
-		require("telescope").setup({})
-		require("telescope").load_extension("fzf")
+		local telescope = require("telescope")
+		telescope.setup({
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				},
+			},
+		})
+		telescope.load_extension("fzf")
 	end,
 }
