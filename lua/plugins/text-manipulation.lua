2534e220a9c99b13e60acdd62966165168eda147
João Pedro <jpedro1580@gmail.com>
2024-05-22 20:20:14 -0300

    feat: makes linting occur only when lsp is active

--- lua/plugins/text-manipulation.lua
+++ lua/plugins/text-manipulation.lua
@@ -1,68 +1,69 @@
 return {
-	{ "echasnovski/mini.surround", version = "*", config = true },
-	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			require("lint").linters_by_ft = {
-				markdown = { "vale" },
-				typescript = { "eslint_d" },
-				javascript = { "eslint_d" },
-			}
-			vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				-- Customize or remove this keymap to your liking
-				"<leader>cf",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "n",
-				desc = "Format buffer",
-			},
-		},
-		-- Everything in opts will be passed to setup()
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				go = { "gofmt" },
-				sh = { "shfmt" },
-				rust = { "rustfmt" },
-				lua = { "stylua" },
-				python = { "black" },
-				javascript = { "prettierd", "prettier" },
-				typescript = { "prettierd", "prettier" },
-				javascriptreact = { "prettierd", "prettier" },
-				typescriptreact = { "prettierd", "prettier" },
-				json = { "prettierd", "prettier" },
-				html = { "prettierd", "prettier" },
-				css = { "prettierd", "prettier" },
-				scss = { "prettierd", "prettier" },
-				sass = { "prettierd", "prettier" },
-				markdown = { "prettierd", "prettier" },
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500, lsp_fallback = true },
-		},
-		init = function()
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"Vonr/align.nvim",
-	},
+  { 'echasnovski/mini.surround', version = '*', config = true },
+  {
+    'mfussenegger/nvim-lint',
+    config = function()
+      require('lint').linters_by_ft = {
+        markdown = { 'vale' },
+        typescript = { 'eslint_d' },
+        javascript = { 'eslint_d' },
+      }
+      vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave' }, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+    event = { 'LspAttach' },
+  },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        '<leader>cf',
+        function()
+          require('conform').format({ async = true, lsp_fallback = true })
+        end,
+        mode = 'n',
+        desc = 'Format buffer',
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        go = { 'gofmt' },
+        sh = { 'shfmt' },
+        rust = { 'rustfmt' },
+        lua = { 'stylua' },
+        python = { 'black' },
+        javascript = { 'prettierd', 'prettier' },
+        typescript = { 'prettierd', 'prettier' },
+        javascriptreact = { 'prettierd', 'prettier' },
+        typescriptreact = { 'prettierd', 'prettier' },
+        json = { 'prettierd', 'prettier' },
+        html = { 'prettierd', 'prettier' },
+        css = { 'prettierd', 'prettier' },
+        scss = { 'prettierd', 'prettier' },
+        sass = { 'prettierd', 'prettier' },
+        markdown = { 'prettierd', 'prettier' },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
+  {
+    'numToStr/Comment.nvim',
+    lazy = false,
+    opts = {},
+  },
+  {
+    'Vonr/align.nvim',
+  },
 }
