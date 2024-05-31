0dfb8b86ac5f23beef6dc4f921db1ae9081f9353
João Pedro <jpedro1580@gmail.com>
2024-05-31 19:02:49 -0300

    feat: fzy native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,22 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.2",
-	dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-	cmd = { "Telescope" },
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({
-			extensions = {
-				fzf = {
-					fuzzy = true, -- false will only do exact matching
-					override_generic_sorter = true, -- override the generic sorter
-					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-				},
-			},
-		})
-		telescope.load_extension("fzf")
-	end,
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-fzy-native.nvim',
+  },
+  cmd = { 'Telescope' },
+  config = function()
+    local telescope = require('telescope')
+    telescope.setup({
+      extensions = {
+        fzy_native = {
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+        },
+      },
+    })
+    telescope.load_extension('fzy_native')
+  end,
 }
