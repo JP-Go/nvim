4935efa59d84bd806822384c10720366371e550d
João Pedro <jpedro1580@gmail.com>
2024-06-07 13:01:59 -0300

    feat: lets try codeium

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,32 +1,33 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'rafamadriz/friendly-snippets',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
       {
-        'L3MON4D3/LuaSnip',
+        "L3MON4D3/LuaSnip",
         build = (function()
           -- Build Step is needed for regex support in snippets
           -- This step is not supported in many windows environments
           -- Remove the below condition to re-enable on windows
-          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
+          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
             return
           end
-          return 'make install_jsregexp'
+          return "make install_jsregexp"
         end)(),
       },
 
-      'saadparwaiz1/cmp_luasnip',
-      'windwp/nvim-autopairs',
+      "saadparwaiz1/cmp_luasnip",
+      "windwp/nvim-autopairs",
     },
     config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      require('nvim-autopairs').setup({})
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      require("nvim-autopairs").setup({})
       luasnip.config.setup()
       cmp.setup({
         snippet = {
@@ -35,52 +36,44 @@ return {
           end,
         },
         completion = {
-          completeopt = 'menu,menuone,noinsert',
+          completeopt = "menu,menuone,noinsert",
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-l>'] = cmp.mapping(function()
-            if require('tabnine.keymaps').has_suggestion() then
-              require('tabnine.keymaps').accept_suggestion()
-            elseif luasnip.expand_or_locally_jumpable() then
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
             end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
             if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
         }),
 
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
-          { name = 'path' },
+          { name = "codeium" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          { name = "path" },
         }, {
-          { name = 'buffer' },
+          { name = "buffer" },
         }),
+        formatting = {
+          format = require("lspkind").cmp_format(),
+        },
       })
     end,
-    event = 'InsertEnter',
+    event = "InsertEnter",
   },
   {
-    'codota/tabnine-nvim',
-    build = './dl_binaries.sh',
-    config = function()
-      require('tabnine').setup({
-        disable_auto_comment = true,
-        accept_keymap = '<Tab>',
-        dismiss_keymap = '<C-]>',
-        debounce_ms = 800,
-        suggestion_color = { gui = '#808080', cterm = 244 },
-        exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
-        log_file_path = nil, -- absolute path to Tabnine log file
-      })
-    end,
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function() end,
   },
 }
