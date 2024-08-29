e73d015ce2b3e28487e089c06cd0307f28e3e4f2
João Pedro <jpedro1580@gmail.com>
2024-08-29 20:15:47 -0300

    feat: remove codeium

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,78 +1,73 @@
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'onsails/lspkind.nvim',
-      {
-        'L3MON4D3/LuaSnip',
-        version = 'v2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
-          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
+    {
+        'hrsh7th/nvim-cmp',
         dependencies = {
-          'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'onsails/lspkind.nvim',
+            {
+                'L3MON4D3/LuaSnip',
+                version = 'v2.*',
+                build = (function()
+                    -- Build Step is needed for regex support in snippets
+                    -- This step is not supported in many windows environments
+                    -- Remove the below condition to re-enable on windows
+                    if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
+                        return
+                    end
+                    return 'make install_jsregexp'
+                end)(),
+                dependencies = {
+                    'rafamadriz/friendly-snippets',
+                },
+            },
+            'saadparwaiz1/cmp_luasnip',
+            'windwp/nvim-autopairs',
         },
-      },
-      'saadparwaiz1/cmp_luasnip',
-      'windwp/nvim-autopairs',
-    },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      require('luasnip.loaders.from_vscode').lazy_load()
-      require('nvim-autopairs').setup({})
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = 'menu,menuone,noinsert',
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable() then
-              luasnip.jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        }),
+        config = function()
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+            require('luasnip.loaders.from_vscode').lazy_load()
+            require('nvim-autopairs').setup({})
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable() then
+                            luasnip.jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
+                }),
 
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
-          { name = 'path' },
-        }, {
-          { name = 'buffer' },
-        }),
-        formatting = {
-          format = require('lspkind').cmp_format(),
-        },
-      })
-    end,
-    event = 'InsertEnter',
-  },
-  {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter',
-    config = function() end,
-  },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'path' },
+                }, {
+                    { name = 'buffer' },
+                }),
+                formatting = {
+                    format = require('lspkind').cmp_format(),
+                },
+            })
+        end,
+        event = 'InsertEnter',
+    },
 }
