5dfbd3d41c0bcbe2c5e4899614bcf886ece74a2e
João Pedro <jpedro1580@gmail.com>
2024-08-29 20:30:17 -0300

    feat: config tabnine

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,1 +1,11 @@
-return { 'codota/tabnine-nvim', build = './dl_binaries.sh' }
+return {
+    'codota/tabnine-nvim',
+    build = './dl_binaries.sh',
+    config = function()
+        require('tabnine').setup({
+            disable_auto_comment = true,
+            debounce_ms = 800,
+            exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
+        })
+    end,
+}
