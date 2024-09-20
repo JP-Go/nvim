8904b37377a4e8227cc4ebed26d9537d332ce0b1
João Pedro <jpedro1580@gmail.com>
2024-09-20 14:50:43 -0300

    refactor(lsp): update tsserver name

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -16,7 +16,7 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = {
           'lua_ls',
-          'tsserver',
+          'ts_ls',
           'cssls',
           'tailwindcss',
           'dockerls',
