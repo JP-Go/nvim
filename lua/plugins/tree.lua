13bd4f346817c20d995030bfb87c291c4bac3c0e
João Pedro <jpedro1580@gmail.com>
2024-09-20 15:33:35 -0300

    feat: new file explorer

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,16 +1,12 @@
 return {
-  'mikavilpas/yazi.nvim',
-  cmd = 'Yazi',
-  opts = {
-    open_for_directories = true
-  },
+  'stevearc/oil.nvim',
+  cmd = 'Oil',
+  opts = {},
   keys = {
     {
-      -- NOTE: this requires a version of yazi that includes
-      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
       '<leader>e',
-      '<cmd>Yazi cwd<cr>',
-      desc = 'Resume the last yazi session',
+      '<cmd>Oil<cr>',
+      desc = 'New file explorer',
     },
   },
 }
