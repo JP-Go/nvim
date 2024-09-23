325673d252d138b020dd5aca3dc0412618c388d4
João Pedro <jpedro1580@gmail.com>
2024-09-23 08:55:45 -0300

    feat: supermaven for ai

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,11 +1,6 @@
 return {
-    'codota/tabnine-nvim',
-    build = './dl_binaries.sh',
-    config = function()
-        require('tabnine').setup({
-            disable_auto_comment = true,
-            debounce_ms = 800,
-            exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
-        })
-    end,
+    {
+      "supermaven-inc/supermaven-nvim",
+      config = true
+    },
 }
