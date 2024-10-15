428e2766a411ed3b364e2a0e465a63350cc8d8b5
João Pedro <jpedro1580@gmail.com>
2024-10-15 08:56:31 -0300

    feat(plugin): adds tmux navigator and new theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,19 +1,30 @@
 return {
-  { 'j-hui/fidget.nvim', opts = {} },
-  { 'blazkowolf/gruber-darker.nvim' },
-  { 'rebelot/kanagawa.nvim' },
-  {
-    'akinsho/bufferline.nvim',
-    config = true,
-    lazy = false,
-    keys = {
-      { '<Tab>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
-      { '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Previous buffer' },
-      { '<leader>bc', '<Cmd>bdelete<CR>', desc = 'Close buffer' },
+    { 'j-hui/fidget.nvim', opts = {} },
+    { 'blazkowolf/gruber-darker.nvim' },
+    {
+        'christoomey/vim-tmux-navigator',
+        lazy = false,
+        keys = {
+            { '<C-h>', '<cmd> TmuxNavigateLeft<CR>', 'Window left' },
+            { '<C-l>', '<cmd> TmuxNavigateRight<CR>', 'Window right' },
+            { '<C-k>', '<cmd> TmuxNavigateUp<CR>', 'Window up' },
+            { '<C-j>', '<cmd> TmuxNavigateDown<CR>', 'Window down' },
+        },
     },
-  },
-  {
-    'stevearc/dressing.nvim',
-    config = true,
-  },
+    { 'rebelot/kanagawa.nvim' },
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+    {
+        'akinsho/bufferline.nvim',
+        config = true,
+        lazy = false,
+        keys = {
+            { '<Tab>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
+            { '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Previous buffer' },
+            { '<leader>bc', '<Cmd>bdelete<CR>', desc = 'Close buffer' },
+        },
+    },
+    {
+        'stevearc/dressing.nvim',
+        config = true,
+    },
 }
