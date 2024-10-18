23f661eb6f069f4f296595759ff9d098c915daa5
João Pedro <jpedro1580@gmail.com>
2024-10-18 13:43:26 -0300

    feat(telescope): add hidden files to find_files picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,27 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    'nvim-telescope/telescope-fzy-native.nvim',
-  },
-  cmd = { 'Telescope' },
-  config = function()
-    local telescope = require('telescope')
-    telescope.setup({
-      extensions = {
-        fzy_native = {
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-        },
-      },
-    })
-    telescope.load_extension('fzy_native')
-  end,
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'nvim-telescope/telescope-fzy-native.nvim',
+    },
+    cmd = { 'Telescope' },
+    config = function()
+        local telescope = require('telescope')
+        telescope.setup({
+            pickers = {
+                find_files = {
+                    find_command = { 'rg', '--files', '--hidden', '--ignore', '--glob', '!**/.git/*' },
+                },
+                extensions = {
+                    fzy_native = {
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true, -- override the file sorter
+                    },
+                },
+            },
+        })
+        telescope.load_extension('fzy_native')
+    end,
 }
