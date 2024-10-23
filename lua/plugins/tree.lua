e100025463b1517adcd6bc9e04907aa66a7a7680
João Pedro <jpedro1580@gmail.com>
2024-10-23 12:13:00 -0300

    feat(tree): use oil as filetree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,16 +1,11 @@
 return {
-  'mikavilpas/yazi.nvim',
-  cmd = 'Yazi',
-  opts = {
-    open_for_directories = true
-  },
-  keys = {
-    {
-      -- NOTE: this requires a version of yazi that includes
-      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-      '<leader>e',
-      '<cmd>Yazi cwd<cr>',
-      desc = 'Resume the last yazi session',
+    'stevearc/oil.nvim',
+    opts = {
+        default_file_explorer = true,
     },
-  },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    keys = {
+        { '<leader>e', '<cmd>Oil<CR>', desc = 'Open file navigator' },
+    },
 }
