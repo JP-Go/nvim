f65a121ed9cbba31677fd00a869c931a42b4acf9
João Pedro <jpedro1580@gmail.com>
2024-11-11 10:26:18 -0300

    feat(tree): change oil to mini.files

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,11 +1,21 @@
 return {
-    'stevearc/oil.nvim',
-    opts = {
-        default_file_explorer = true,
-    },
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    keys = {
-        { '<leader>e', '<cmd>Oil<CR>', desc = 'Open file navigator' },
+    {
+        'echasnovski/mini.files',
+        version = '*',
+        dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+        config = {
+            mappings = {
+                go_in_plus = '<CR>',
+            },
+        },
+        keys = {
+            {
+                '<leader>e',
+                function()
+                    require('mini.files').open()
+                end,
+                desc = 'Open file navigator',
+            },
+        },
     },
 }
