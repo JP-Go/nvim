64f3c6b9a31173a7073e3e93fc71f7229aff792b
João Pedro <jpedro1580@gmail.com>
2024-12-10 20:11:34 -0300

    feat(editor): add hapoon

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,6 +2,39 @@ return {
     { 'echasnovski/mini.surround', version = '*', config = true },
     { 'echasnovski/mini.pairs', version = '*', config = true },
     {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local harpoon = require('harpoon')
+            harpoon:setup()
+            local conf = require('telescope.config').values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require('telescope.pickers')
+                    .new({}, {
+                        prompt_title = 'Harpoon',
+                        finder = require('telescope.finders').new_table({
+                            results = file_paths,
+                        }),
+                        previewer = conf.file_previewer({}),
+                        sorter = conf.generic_sorter({}),
+                    })
+                    :find()
+            end
+            vim.keymap.set('n', '<leader>a', function()
+                harpoon:list():add()
+            end, { desc = 'Add buffer to harpoon list' })
+            vim.keymap.set('n', '<C-e>', function()
+                toggle_telescope(harpoon:list())
+            end)
+        end,
+    },
+    {
         'akinsho/toggleterm.nvim',
         version = '*',
         event = 'VeryLazy',
