249089406c2510f6d0a281ab1e9dbef035fbd13f
João Pedro <jpedro1580@gmail.com>
2025-03-24 07:51:09 -0300

    feat(editor): adds text case change plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,6 +2,30 @@ return {
     { 'echasnovski/mini.surround', version = '*', config = true, event = { 'InsertEnter' } },
     { 'echasnovski/mini.pairs', version = '*', config = true, event = { 'InsertEnter' } },
     {
+        'johmsalas/text-case.nvim',
+        dependencies = { 'nvim-telescope/telescope.nvim' },
+        config = function()
+            require('textcase').setup({})
+            require('telescope').load_extension('textcase')
+        end,
+        keys = {
+            'ga', -- Default invocation prefix
+            { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
+        },
+        cmd = {
+            -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+            'Subs',
+            'TextCaseOpenTelescope',
+            'TextCaseOpenTelescopeQuickChange',
+            'TextCaseOpenTelescopeLSPChange',
+            'TextCaseStartReplacingCommand',
+        },
+        -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+        -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+        -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+        lazy = false,
+    },
+    {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
