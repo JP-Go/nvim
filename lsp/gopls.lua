c2e0b8630f64de5c20e2e33c7d13c073950f609d
João Pedro <jpedro1580@gmail.com>
2025-04-13 21:23:40 -0300

    feat(lsp): adds go lsp config

---
+++ lsp/gopls.lua
@@ -0,0 +1,8 @@
+local util = require('lspconfig.util')
+
+return {
+    cmd = { 'gopls' },
+    filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+    root_markers = { 'go.mod', 'go.work', '.git' },
+    single_file_support = true,
+}
