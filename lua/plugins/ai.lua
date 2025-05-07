0adde6bcc010445c15045cf56d13f3af24cf8f84
João Pedro <jpedro1580@gmail.com>
2025-05-06 22:24:18 -0300

    feat(ai): add avante nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,4 +3,33 @@ return {
         'supermaven-inc/supermaven-nvim',
         config = true,
     },
+    {
+        'yetone/avante.nvim',
+        event = 'VeryLazy',
+        version = false, -- Never set this value to "*"! Never!
+        opts = {
+            -- add any opts here
+            -- for example
+            provider = 'copilot',
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = 'make',
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'stevearc/dressing.nvim',
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            --- The below dependencies are optional,
+            'zbirenbaum/copilot.lua', -- for providers='copilot'
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { 'markdown', 'Avante' },
+                },
+                ft = { 'markdown', 'Avante' },
+            },
+        },
+    },
 }
