72edb6a0d361fe9895d1077efdb9625287bb0c2b
João Pedro <jpedro1580@gmail.com>
2025-05-13 08:12:15 -0300

    feat(ai): add mcp client for neovim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -11,12 +11,35 @@ return {
             require('copilot').setup()
             require('avante').setup({
                 provider = 'copilot',
+                system_prompt = function()
+                    local hub = require('mcphub').get_hub_instance()
+                    return hub:get_active_servers_prompt()
+                end,
+                -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+                custom_tools = function()
+                    return {
+                        require('mcphub.extensions.avante').mcp_tool(),
+                    }
+                end,
             })
         end,
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
         build = 'make',
         -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
         dependencies = {
+            {
+
+                'ravitemer/mcphub.nvim',
+                build = 'bundled_build.lua',
+                opts = {
+                    use_bundled_binary = true,
+                    extensions = {
+                        avante = {
+                            make_slash_commands = true,
+                        },
+                    },
+                },
+            },
             'nvim-treesitter/nvim-treesitter',
             'stevearc/dressing.nvim',
             'nvim-lua/plenary.nvim',
