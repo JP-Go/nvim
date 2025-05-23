147e1a395547a65bae01bccdca77fd196ea55914
João Pedro <jpedro1580@gmail.com>
2025-05-23 11:12:15 -0300

    feat(ai): adds Ai completion from avante

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,10 +1,28 @@
 return {
     {
         'saghen/blink.cmp',
-        dependencies = { 'rafamadriz/friendly-snippets' },
+        dependencies = { 'rafamadriz/friendly-snippets', 'Kaiser-Yang/blink-cmp-avante' },
         version = '1.*',
 
         opts = {
+            sources = {
+                default = {
+                    'avante',
+                    'lsp',
+                    'path',
+                    'luasnip',
+                    'buffer',
+                },
+                providers = {
+                    avante = {
+                        module = 'blink-cmp-avante',
+                        name = 'Avante',
+                        opts = {
+                            -- options for blink-cmp-avante
+                        },
+                    },
+                },
+            },
             keymap = { preset = 'enter' },
             completion = {
                 menu = { border = 'single' },
