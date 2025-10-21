da2d85bf34e492b255593923494e2c94c53127dd
João Pedro <jpedro1580@gmail.com>
2025-10-21 10:20:26 -0300

    feat(ai): avante build for win32 environments

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,67 +1,73 @@
 return {
-    {
-        'yetone/avante.nvim',
-        event = 'VeryLazy',
-        version = false, -- Never set this value to "*"! Never!
-        config = function()
-            require('copilot').setup()
-            require('avante').setup({
-                provider = 'copilot',
-                behavior = {
-                    enable_cursor_planning_mode = true,
-                },
-                providers = {
-                    copilot = {
-                        model = 'claude-3.5-sonnet',
-                        extra_request_body = {
-                            temperature = 0,
-                            max_tokens = 8192,
-                        },
-                    },
-                },
-                system_prompt = function()
-                    local hub = require('mcphub').get_hub_instance()
-                    return hub:get_active_servers_prompt()
-                end,
-                -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
-                custom_tools = function()
-                    return {
-                        require('mcphub.extensions.avante').mcp_tool(),
-                    }
-                end,
-            })
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    auto_suggestions_provider = nil,
+    config = function()
+      require('copilot').setup({
+        should_attach = function(_, bufname)
+          if string.match(bufname, "env") then
+            return false
+          end
+          return true
         end,
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = 'make',
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-        dependencies = {
-            {
-
-                'ravitemer/mcphub.nvim',
-                build = 'bundled_build.lua',
-                opts = {
-                    use_bundled_binary = true,
-                    extensions = {
-                        avante = {
-                            make_slash_commands = true,
-                        },
-                    },
-                },
-            },
-            'nvim-treesitter/nvim-treesitter',
-            'stevearc/dressing.nvim',
-            'nvim-lua/plenary.nvim',
-            'MunifTanjim/nui.nvim',
-            --- The below dependencies are optional,
-            'zbirenbaum/copilot.lua', -- for providers='copilot'
-            {
-                -- Make sure to set this up properly if you have lazy=true
-                'MeanderingProgrammer/render-markdown.nvim',
-                opts = {
-                    file_types = { 'markdown', 'Avante' },
-                },
-                ft = { 'markdown', 'Avante' },
-            },
+        model = 'claude-3.5-sonnet',
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 8192,
         },
+      })
+      require('avante').setup({
+        provider = 'copilot',
+        behavior = {
+          enable_cursor_planning_mode = true,
+        },
+        system_prompt = function()
+          local hub = require('mcphub').get_hub_instance()
+          return hub:get_active_servers_prompt()
+        end,
+        -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+        custom_tools = function()
+          return {
+            require('mcphub.extensions.avante').mcp_tool(),
+          }
+        end,
+      })
+    end,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      {
+
+        'ravitemer/mcphub.nvim',
+        build = 'npm install -g mcp-hub@latest',
+        opts = {
+          use_bundled_binary = false,
+          extensions = {
+            avante = {
+              make_slash_commands = true,
+            },
+          },
+        },
+      },
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
     },
+  },
 }
