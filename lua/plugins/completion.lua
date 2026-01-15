5b022d896db54ab288e841ce58415cb038980ff5
João Pedro <jpedro1580@gmail.com>
2026-01-15 13:01:50 -0300

    refactor(completion): adds lazydev completions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,43 +1,50 @@
 return {
-    {
-        'saghen/blink.cmp',
-        dependencies = { 'rafamadriz/friendly-snippets', 'Kaiser-Yang/blink-cmp-avante' },
-        version = '1.*',
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets', 'Kaiser-Yang/blink-cmp-avante' },
+    version = '1.*',
 
-        opts = {
-            sources = {
-                default = {
-                    'avante',
-                    'lsp',
-                    'path',
-                    'buffer',
-                },
-                providers = {
-                    avante = {
-                        module = 'blink-cmp-avante',
-                        name = 'Avante',
-                        opts = {
-                            -- options for blink-cmp-avante
-                        },
-                    },
-                },
-            },
-            keymap = { preset = 'enter' },
-            completion = {
-                menu = { border = 'single' },
-                list = {
-                    selection = {
-                        preselect = function(ctx)
-                            return ctx.mode ~= 'cmdline'
-                        end,
-                        auto_insert = true,
-                    },
-                },
-                documentation = {
-                    window = { border = 'single' },
-                },
-            },
-            signature = { enabled = true, window = { border = 'single' } },
+    opts = {
+      sources = {
+        default = {
+          'lazydev',
+          'avante',
+          'lsp',
+          'path',
+          'buffer',
         },
+        providers = {
+          avante = {
+            module = 'blink-cmp-avante',
+            name = 'Avante',
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          }
+        },
+      },
+      keymap = { preset = 'enter' },
+      completion = {
+        menu = { border = 'single' },
+        list = {
+          selection = {
+            preselect = function(ctx)
+              return ctx.mode ~= 'cmdline'
+            end,
+            auto_insert = true,
+          },
+        },
+        documentation = {
+          window = { border = 'single' },
+        },
+      },
+      signature = { enabled = true, window = { border = 'single' } },
     },
+  },
 }
