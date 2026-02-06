d7823088300a3c2fcf37c9188ddb074110508ce4
João Pedro <jpedro1580@gmail.com>
2026-02-06 13:43:30 -0300

    refactor(formating): add biome as option for formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,44 +1,44 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            '<leader>cf',
-            function()
-                require('conform').format({ async = true, lsp_fallback = true })
-            end,
-            mode = 'n',
-            desc = 'Format buffer',
-        },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      '<leader>cf',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = 'n',
+      desc = 'Format buffer',
     },
-    -- Everything in opts will be passed to setup()
-    opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-            csharp = { 'csharpier' },
-            go = { 'gofmt' },
-            sh = { 'shfmt' },
-            rust = { 'rustfmt' },
-            lua = { 'stylua' },
-            python = { 'black' },
-            php = { 'pint' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescript = { 'prettierd', 'prettier', stop_after_first = true },
-            javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-            typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-            json = { 'prettierd', 'prettier', stop_after_first = true },
-            html = { 'prettierd', 'prettier', stop_after_first = true },
-            css = { 'prettierd', 'prettier', stop_after_first = true },
-            scss = { 'prettierd', 'prettier', stop_after_first = true },
-            sass = { 'prettierd', 'prettier', stop_after_first = true },
-            markdown = { 'prettierd', 'prettier', stop_after_first = true },
-        },
-        -- Set up format-on-save
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      csharp = { 'csharpier' },
+      go = { 'gofmt' },
+      sh = { 'shfmt' },
+      rust = { 'rustfmt' },
+      lua = { 'stylua' },
+      python = { 'black' },
+      php = { 'pint' },
+      javascript = { 'biome', 'biome-organize-imports', 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'biome', 'biome-organize-imports', 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'biome', 'biome-organize-imports', 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'biome', 'biome-organize-imports', 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      sass = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+      markdown = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
     },
-    init = function()
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
