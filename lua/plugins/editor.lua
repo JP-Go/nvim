98ca422a6d4e85d2fefec3799c87df299f79d835
João Pedro <jpedro1580@gmail.com>
2026-02-21 11:07:57 -0300

    refactor: new treesitter api

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,258 +1,229 @@
 return {
-    { 'echasnovski/mini.surround', version = '*', config = true, event = { 'InsertEnter' } },
-    { 'echasnovski/mini.pairs', version = '*', config = true, event = { 'InsertEnter' } },
-    {
-        'ThePrimeagen/refactoring.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        lazy = false,
-        opts = {},
+  { 'echasnovski/mini.surround', version = '*', config = true, event = { 'InsertEnter' } },
+  { 'echasnovski/mini.pairs',    version = '*', config = true, event = { 'InsertEnter' } },
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
     },
-    {
-        'johmsalas/text-case.nvim',
-        dependencies = { 'nvim-telescope/telescope.nvim' },
-        config = function()
-            require('textcase').setup({})
-            require('telescope').load_extension('textcase')
+    lazy = false,
+    opts = {},
+  },
+  {
+    'johmsalas/text-case.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('textcase').setup({})
+      require('telescope').load_extension('textcase')
+    end,
+    keys = {
+      'ga', -- Default invocation prefix
+      { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
+    },
+    cmd = {
+      'Subs',
+      'TextCaseOpenTelescope',
+      'TextCaseOpenTelescopeQuickChange',
+      'TextCaseOpenTelescopeLSPChange',
+      'TextCaseStartReplacingCommand',
+    },
+    -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+    -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+    -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+    lazy = false,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    lazy = false,
+    config = function()
+      require('nvim-treesitter').install({ "c", "javascript", "typescript", "sql", "prisma", "html", "jsx", "rust", "lua" })
+    end,
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = true,
+    event = { 'InsertEnter' },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('treesitter-context').setup()
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+    cmd = { 'RenderMarkdown' },
+    opts = {},
+  },
+  {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = {
+      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", 'Reveal undotree' },
+    },
+  },
+  { 'echasnovski/mini.comment', version = '*', event = { 'BufReadPost' } },
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      dashboard = { enabled = true },
+      explorer = { enabled = true, replace_netrw = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      lazygit = { enabled = true },
+      terminal = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+    keys = {
+      {
+        '<c-/>',
+        function()
+          Snacks.terminal()
         end,
-        keys = {
-            'ga', -- Default invocation prefix
-            { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
-        },
-        cmd = {
-            'Subs',
-            'TextCaseOpenTelescope',
-            'TextCaseOpenTelescopeQuickChange',
-            'TextCaseOpenTelescopeLSPChange',
-            'TextCaseStartReplacingCommand',
-        },
-        -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
-        -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
-        -- available after the first executing of it or after a keymap of text-case.nvim has been used.
-        lazy = false,
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = {
-                    'c',
-                    'lua',
-                    'vim',
-                    'vimdoc',
-                    'query',
-                    'sql',
-                    'bash',
-                    'javascript',
-                    'typescript',
-                    'css',
-                    'html',
-                    'tsx',
-                    'vue',
-                    'prisma',
-                },
-
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = false,
-
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-            })
+        desc = 'Toggle Terminal',
+      },
+      {
+        '<leader>gg',
+        function()
+          Snacks.lazygit()
         end,
-    },
-    {
-        'windwp/nvim-ts-autotag',
-        config = true,
-        event = { 'InsertEnter' },
-    },
-    {
-        'nvim-treesitter/nvim-treesitter-context',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-            require('treesitter-context').setup()
+        desc = 'Lazygit',
+      },
+      {
+        '<leader>ff',
+        function()
+          Snacks.picker.files()
         end,
+        desc = 'Find Files',
+      },
+      {
+        '<leader>fw',
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = 'Find word',
+      },
+      {
+        '<leader>fg',
+        function()
+          Snacks.picker.git_files()
+        end,
+        desc = 'Find Git Files',
+      },
+      {
+        '<leader>fp',
+        function()
+          Snacks.picker.projects()
+        end,
+        desc = 'Projects',
+      },
+      {
+        '<leader>fr',
+        function()
+          Snacks.picker.recent()
+        end,
+        desc = 'Recent',
+      },
+      {
+        'gd',
+        function()
+          Snacks.picker.lsp_definitions()
+        end,
+        desc = 'Goto Definition',
+      },
+      {
+        'gD',
+        function()
+          Snacks.picker.lsp_declarations()
+        end,
+        desc = 'Goto Declaration',
+      },
+      {
+        'gr',
+        function()
+          Snacks.picker.lsp_references()
+        end,
+        nowait = true,
+        desc = 'References',
+      },
+      {
+        'gI',
+        function()
+          Snacks.picker.lsp_implementations()
+        end,
+        desc = 'Goto Implementation',
+      },
+      {
+        'gy',
+        function()
+          Snacks.picker.lsp_type_definitions()
+        end,
+        desc = 'Goto T[y]pe Definition',
+      },
+      {
+        '<leader>ss',
+        function()
+          Snacks.picker.lsp_symbols()
+        end,
+        desc = 'LSP Symbols',
+      },
+      {
+        '<leader>sS',
+        function()
+          Snacks.picker.lsp_workspace_symbols()
+        end,
+        desc = 'LSP Workspace Symbols',
+      },
+      {
+        '<leader>/',
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = 'Grep',
+      },
+      {
+        '<leader>e',
+        function()
+          Snacks.explorer()
+        end,
+        desc = 'File Explorer',
+      },
+      {
+        '<leader><leader>',
+        function()
+          Snacks.picker.smart()
+        end,
+        desc = 'File Explorer',
+      },
     },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-        cmd = { 'RenderMarkdown' },
-        opts = {},
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = true,
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
     },
-    {
-        'jiaoshijie/undotree',
-        dependencies = 'nvim-lua/plenary.nvim',
-        config = true,
-        keys = {
-            { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", 'Reveal undotree' },
-        },
-    },
-    { 'echasnovski/mini.comment', version = '*', event = { 'BufReadPost' } },
-    {
-        'folke/snacks.nvim',
-        priority = 1000,
-        lazy = false,
-        ---@type snacks.Config
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-            dashboard = { enabled = true },
-            explorer = { enabled = true, replace_netrw = true },
-            input = { enabled = true },
-            picker = { enabled = true },
-            notifier = { enabled = true },
-            quickfile = { enabled = true },
-            lazygit = { enabled = true },
-            terminal = { enabled = true },
-            statuscolumn = { enabled = true },
-            words = { enabled = true },
-        },
-        keys = {
-            {
-                '<c-/>',
-                function()
-                    Snacks.terminal()
-                end,
-                desc = 'Toggle Terminal',
-            },
-            {
-                '<leader>gg',
-                function()
-                    Snacks.lazygit()
-                end,
-                desc = 'Lazygit',
-            },
-            {
-                '<leader>ff',
-                function()
-                    Snacks.picker.files()
-                end,
-                desc = 'Find Files',
-            },
-            {
-                '<leader>fw',
-                function()
-                    Snacks.picker.grep()
-                end,
-                desc = 'Find word',
-            },
-            {
-                '<leader>fg',
-                function()
-                    Snacks.picker.git_files()
-                end,
-                desc = 'Find Git Files',
-            },
-            {
-                '<leader>fp',
-                function()
-                    Snacks.picker.projects()
-                end,
-                desc = 'Projects',
-            },
-            {
-                '<leader>fr',
-                function()
-                    Snacks.picker.recent()
-                end,
-                desc = 'Recent',
-            },
-            {
-                'gd',
-                function()
-                    Snacks.picker.lsp_definitions()
-                end,
-                desc = 'Goto Definition',
-            },
-            {
-                'gD',
-                function()
-                    Snacks.picker.lsp_declarations()
-                end,
-                desc = 'Goto Declaration',
-            },
-            {
-                'gr',
-                function()
-                    Snacks.picker.lsp_references()
-                end,
-                nowait = true,
-                desc = 'References',
-            },
-            {
-                'gI',
-                function()
-                    Snacks.picker.lsp_implementations()
-                end,
-                desc = 'Goto Implementation',
-            },
-            {
-                'gy',
-                function()
-                    Snacks.picker.lsp_type_definitions()
-                end,
-                desc = 'Goto T[y]pe Definition',
-            },
-            {
-                '<leader>ss',
-                function()
-                    Snacks.picker.lsp_symbols()
-                end,
-                desc = 'LSP Symbols',
-            },
-            {
-                '<leader>sS',
-                function()
-                    Snacks.picker.lsp_workspace_symbols()
-                end,
-                desc = 'LSP Workspace Symbols',
-            },
-            {
-                '<leader>/',
-                function()
-                    Snacks.picker.grep()
-                end,
-                desc = 'Grep',
-            },
-            {
-                '<leader>e',
-                function()
-                    Snacks.explorer()
-                end,
-                desc = 'File Explorer',
-            },
-            {
-                '<leader><leader>',
-                function()
-                    Snacks.picker.smart()
-                end,
-                desc = 'File Explorer',
-            },
-        },
-    },
-    {
-        'lewis6991/gitsigns.nvim',
-        config = true,
-        opts = {
-            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-            current_line_blame_opts = {
-                virt_text = true,
-                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                delay = 1000,
-                ignore_whitespace = false,
-                virt_text_priority = 100,
-                use_focus = true,
-            },
-        },
-    },
+  },
 }
