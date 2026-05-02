454d005ef126c1d142843074325fa8d4b4fa6018
João Pedro <jpedro1580@gmail.com>
2026-05-02 17:44:17 -0300

    refactor: remove fold and highlight on yank

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,10 +3,15 @@ local autocmd = core.add_autocmd
 
 autocmd("FileType", {
   pattern = {'javascript',"typescript",'javascriptreact',"typescriptreact",'go',"gomod","gosum",'lua'},
-  callback = function() 
+  callback = function()
     vim.treesitter.start()
-    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    vim.wo[0][0].foldmethod = 'expr'
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+})
+
+autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
   end
 })
