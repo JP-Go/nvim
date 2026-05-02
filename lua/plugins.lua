dfe60518d09b6706027ef55bde238c65b8c106b3
João Pedro <jpedro1580@gmail.com>
2026-05-02 18:00:09 -0300

    feat: configure dashboard

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,7 +109,29 @@ require('nvim-treesitter').install({
 require('snacks').setup({
     bufdelete = { enabled = true },
     bigfile = { enabled = true },
-    dashboard = { enabled = false },
+    dashboard = {
+        sections = {
+            { section = 'header' },
+            { section = 'keys', gap = 1, padding = 1 },
+            { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+            { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+            {
+                pane = 2,
+                icon = ' ',
+                title = 'Git Status',
+                section = 'terminal',
+                enabled = function()
+                    return Snacks.git.get_root() ~= nil
+                end,
+                cmd = 'git status --short --branch --renames',
+                height = 5,
+                padding = 1,
+                ttl = 5 * 60,
+                indent = 3,
+            },
+            -- { section = 'startup' },
+        },
+    },
     explorer = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
