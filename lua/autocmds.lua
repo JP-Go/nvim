5e6f580fdc9536b9a47eb9bb59890d585543e18f
João Pedro <jpedro1580@gmail.com>
2026-05-02 17:52:30 -0300

    chore: formatting

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,17 +1,17 @@
-local core = require("core")
+local core = require('core')
 local autocmd = core.add_autocmd
 
-autocmd("FileType", {
-  pattern = {'javascript',"typescript",'javascriptreact',"typescriptreact",'go',"gomod","gosum",'lua'},
-  callback = function()
-    vim.treesitter.start()
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end
+autocmd('FileType', {
+    pattern = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'go', 'gomod', 'gosum', 'lua' },
+    callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
 })
 
-autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank()
-  end
+autocmd('TextYankPost', {
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
