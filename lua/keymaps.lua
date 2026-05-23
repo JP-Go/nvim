b6d76f0debd713fd5a1f3a724c3377755a0bf8b8
João Pedro <jpedro1580@gmail.com>
2026-05-22 23:10:32 -0300

    feat: add common keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,3 @@
+Core.add_keymap('n', '<ESC>', '<cmd>set nohlsearch<cr>')
+Core.add_keymap('n', '<Tab>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+Core.add_keymap('n', '<S-Tab>', '<cmd>bprev<cr>', { desc = 'Previous Buffer' })
