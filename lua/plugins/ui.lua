5bcd6986fe6243f22920244b0b212d4ac2e8ac8e
João Pedro <jpedro1580@gmail.com>
2026-05-26 22:08:38 -0300

    feat: new theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,7 +12,7 @@ Core.add_keymap('n', '<S-Tab>', '<cmd>BufferPrevious<cr>', { desc = 'Previous Bu
 Core.add_plugin({ Core.gh('sontungexpt/better-diagnostic-virtual-text') })
 
 Core.add_autocmd('LspAttach', {
-    pattern = '*',
+    pattern = { 'go', 'lua', 'gomod', 'gosum', 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' },
     callback = function()
         require('better-diagnostic-virtual-text').setup()
     end,
@@ -23,3 +23,9 @@ Core.add_plugin({ Core.gh('OXY2DEV/markview.nvim') })
 require('markview').setup()
 
 Core.add_plugin({ Core.gh('chrisgrieser/nvim-rulebook') })
+
+Core.add_plugin({ Core.gh('uhs-robert/oasis.nvim') })
+
+require('oasis').setup()
+
+vim.cmd.colorscheme('oasis-desert')
