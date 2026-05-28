bb9c43ceab8928d5087acca3283c8e9d7f61324c
João Pedro <jpedro1580@gmail.com>
2026-05-28 19:51:39 -0300

    chore: come back

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,3 @@
+Core.add_keymap('n', '<ESC>', '<cmd>set nohlsearch<cr>', { desc = 'Clear highlights' })
 Core.add_keymap('n', '<Tab>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 Core.add_keymap('n', '<S-Tab>', '<cmd>bprev<cr>', { desc = 'Previous Buffer' })
