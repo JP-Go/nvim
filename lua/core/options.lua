893444e4edd7a268ee0e5a76a2262205f18b92b1
João Pedro <jpedro1580@gmail.com>
2026-07-22 09:35:23 -0300

    chore: format and show line numbers

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,22 +1,22 @@
 local opts = {
-    termguicolors = true,
-    backup = false,
-    swapfile = false,
-    undofile = true,
-    splitright = true,
-    softtabstop = 4,
-    tabstop = 4,
-    shiftwidth = 4,
-    laststatus = 3,
-    cursorline = true,
-    shiftround = true,
-    expandtab = true,
-    cmdheight = 2,
-    textwidth = 80,
-    number = false,
-    hidden = true,
+  termguicolors = true,
+  backup = false,
+  swapfile = false,
+  undofile = true,
+  splitright = true,
+  softtabstop = 4,
+  tabstop = 4,
+  shiftwidth = 4,
+  laststatus = 3,
+  cursorline = true,
+  shiftround = true,
+  expandtab = true,
+  cmdheight = 2,
+  textwidth = 80,
+  number = true,
+  hidden = true,
 }
 
 for k, v in pairs(opts) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
